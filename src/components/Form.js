6918ea8b162c3cd01732b413ff2a2ae911c2c6c5
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-02 18:27:45 +0200

    added button in form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Form = () => {
   const [name, setName] = useState('')
+  const navigate = useNavigate()
+
+  const onBackButtonClick = () => {
+    navigate(-1);
+  }
 
   return (
-    <form>
-      <h1>The state is: {name}</h1>
-      <input
-        type="text"
-        onChange={(event) => setName(event.target.value)}
-        value={name} />
-    </form>
+    <div className="form-container">
+      <form>
+        <h1>The state is: {name}</h1>
+        <input
+          type="text"
+          onChange={(event) => setName(event.target.value)}
+          value={name} />
+      </form>
+      <button type="button" onClick={onBackButtonClick}>Go back</button>
+    </div>
   );
 }
