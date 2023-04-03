e875e847b2e169f4e33fc5848dc42fdbd9c593c6
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-03 19:24:36 +0200

    update form and added component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,16 @@
 import React from 'react'
 
-export const Form = (props) => {
-  const handleSubmit = (event) => {
-    props.set(event.targe.usernameInput.value);
-  }
-
+export const Form = () => {
   return (
-    <div className="main-container">
-      <h2>Enter your name</h2>
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          name="usernameInput"
-          type="text"
-          required />
-      </form>
-    </div>
+    <form>
+      <fieldset>
+        <legend>Create an account</legend>
+        <input type="text" placeholder="Name" required />
+        <input type="email" placeholder="E-mail" required />
+        <input type="tel" placeholder="Phone Number" required />
+        <button type="submit">Submit</button>
+      </fieldset>
+    </form>
+
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,10 @@ export const Header = () => {
   return (
     <header>
       <nav>
-
         <ul className="navLinks">
-          <li><a href="">Home</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">Contact</a></li>
+          <li><a href="">CONTACT</a></li>
+          <li><a href="">FAQ</a></li>
+          <li><a href="">ABOUT</a></li>
         </ul>
       </nav>
     </header>
