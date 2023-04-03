f6f8c4b450a61eb79d9e0b58b86ef1bdc5b66d50
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-03 20:44:55 +0200

    added styling to button without result

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,12 @@ export const Form = () => {
   return (
     <FormDesign>
       <form className="orderCard">
-        <label htmlFor="orderHere">Your contacts
+        <label htmlFor="orderHere">Your page log-in
           <input type="text" placeholder="Name" required />
           <input type="email" placeholder="E-mail" required />
           <input type="tel" placeholder="Phone Number" required />
         </label>
-        <button type="submit">Submit</button>
+        <button className="button" type="submit">Submit</button>
       </form>
     </FormDesign>
   )
