7008d41e85909ebc91df9639a76e651b57b7204f
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-03 12:49:36 +0200

    header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-export const Header = () => {
+export const Header = ({ title, link, link1, link2, link3 }) => {
   return (
     <header>
-      <p>This is my header.</p>
+      <header>
+        <h1>{title}</h1>
+        <nav>
+          <ul>
+            <li><a>{link}</a></li>
+            <li><a>{link1}</a></li>
+            <li><a>{link2}</a></li>
+            <li><a>{link3}</a></li>
+          </ul>
+        </nav>
+      </header>
     </header>
   );
 }
