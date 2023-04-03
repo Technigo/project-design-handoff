0e1f9e8340fd4aa5b86196f5750377710d0ce458
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-03 14:21:42 +0200

    styled component button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,9 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-
-display: inline-block;
-border-radius: 3px;
-padding: 0.5rem 0;
-margin: 0.5rem 1rem;
-width: 11rem;
-background: transparent;
+background-color: blue;
+border-radius: 8px;
 color: white;
-border: 2px solid white;
+font-size: 30px;
+padding: 8px 16px;
 `
