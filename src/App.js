662ad76c554b09e8973196838c537aabad47098a
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-04 12:39:31 +0200

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Header } from 'components/Header'
-import { Form } from 'components/Form'
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <main>
-        <Routes>
-          <Route path="/" element={<Header />} />
-          <Route path="/form" element={<Form />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <div>
+      <h1>Hello</h1>
+    </div>
   )
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav>
+      <ul className="navLinks">
+        <li><a href="">CONTACT</a></li>
+        <li><a href="">FAQ</a></li>
+        <li><a href="">ABOUT</a></li>
+      </ul>
+    </nav>
+  )
+}
