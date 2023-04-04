1202805ee95414f0e61041430db0cf2a8c5776ae
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-04-04 16:16:37 +0200

    Some fixes with navigation due to Routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { NavbarContainer } from '../styles/Navbar.style';
+import {
+  LeftContainer,
+  NavbarContainer,
+  NavbarExtendedContainer,
+  NavbarInnerContainer,
+  RightContainer,
+  NavbarLinkContainer,
+  NavbarLink
+} from '../styles/Navbar.style';
 
 const Navbar = () => {
   return (
     <NavbarContainer>
+      <NavbarInnerContainer>
+        <LeftContainer>
+          <NavbarLinkContainer>
+            <NavbarLink to="/"> Home</NavbarLink>
+            <NavbarLink to="/"> Activities</NavbarLink>
+            <NavbarLink to="/"> Contact</NavbarLink>
+          </NavbarLinkContainer>
+        </LeftContainer>
+        <RightContainer>
+LOGO
+        </RightContainer>
+      </NavbarInnerContainer>
+      <NavbarExtendedContainer />
+      {/* OUTCOMMENTED FOR NOW
+      <NavbarExtendedContainer>
+      </NavbarExtendedContainer> */}
       <ul>
         <li>Hello</li>
         <li>You</li>
--- src/styles/Navbar.style.js
+++ src/styles/Navbar.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const NavbarContainer = styled.nav`
 width: 100%;
@@ -7,4 +8,48 @@ background-color: black;
 display: flex;
 flex-direction: column;
 color: white;
+`;
+
+export const LeftContainer = styled.div`
+flex: 70%;
+display: flex;
+align-items: center;
+padding-left: 5%;
+background-color: red;
+`;
+
+export const RightContainer = styled.div`
+flex: 30%;
+display: flex;
+align-items: flex-end;
+padding-left: 50px;
+background-color: salmon;
+`;
+
+export const NavbarInnerContainer = styled.div`
+width: 100%;
+height: 80px;
+flex: display;
+`;
+
+export const NavbarLinkContainer = styled.div`
+display: flex;
+`;
+
+export const NavbarLink = styled(Link)`
+color: white;
+font-size: x-large;
+font-family: Roboto;
+text-decoration: none;
+margin: 10px;
+`;
+
+/* Below is outcommented for now as I don't have a logo yet */
+// export const Logo = styled.img`
+// margin: 10px;
+// max-width: 180px;
+// height: auto;
+// `;
+
+export const NavbarExtendedContainer = styled.div`
 `;
