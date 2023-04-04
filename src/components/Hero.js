8ddbc6caf653c9aae42404cdbb54cfd6924ab200
tess.aquilon <tess.aquilon@gmail.com>
2023-04-04 15:39:30 +0200

    added a hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Hero = () => {
+    return (
+        <>
+            <h1>Breath in breath out</h1>
+            <p>Yogistudio offers an overall concept in self-healing with a focus on calm and therapeutic forms of yoga.</p>
+            <label>{id}
+              <btn type="button" id="bookAClass">Book a class</btn>
+            </label>
+        </>
+    )
+}
