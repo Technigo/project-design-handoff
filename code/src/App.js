dd73265bf5bbd57178cee3c2db3e13b406c1955e
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-04-04 21:57:44 +0200

    first in

---
+++ code/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeroSection } from 'components/Hero'
+
+export const App = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+}
---
+++ code/src/components/Hero.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import heroImage from '../images/hero-image.png';
+
+export const HeroSection = () => {
+  return (
+    <Container>
+      <HeaderText>Header Text</HeaderText>
+      <SubText>Smaller Text Box</SubText>
+      <Button>Try for Free</Button>
+    </Container>
+  );
+};
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  widht: 100vw;
+  height: 100vh;
+  background-image: url(${heroImage});
+  background-size: cover;
+  background-position: center;
+`;
+
+const HeaderText = styled.h1`
+  font-size: 4rem;
+  font-weight: bold;
+  color: #fff;
+`;
+
+const SubText = styled.p`
+  font-size: 2rem;
+  color: #fff;
+`;
+
+const Button = styled.button`
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+  border: none;
+  background-color: #f44336;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #fff;
+    color: #f44336;
+  }
+`;
