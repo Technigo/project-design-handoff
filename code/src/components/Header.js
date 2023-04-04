961e79b95b4c2541062b13025e670cb96a0acfd9
Frid Matilda <matilda.x.frid@scania.com>
2023-04-04 17:42:11 +0200

    hero fix

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../assets/logos/Vector.png';
+import logoMedium from '../assets/logos/Vector-medium.png';
+// import logoSmall from '../assets/logos/Vector-small.png';
+import logoMini from '../assets/logos/Vector-mini.png';
 import menuicon from '../assets/icons/Menu.png';
 import searchicon from '../assets/icons/Search.png';
 import './Header.css';
 
 const Header = () => {
   const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const isMobile = screenSize <= 640;
   const isDesktop = screenSize >= 1025;
-  console.log('screenSize', screenSize)
-  console.log('isDesktop', isDesktop)
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -20,7 +22,8 @@ const Header = () => {
     <>
       {!isDesktop && (
         <div className="Header">
-          <img src={logo} alt="Gracious moments logo" />
+          {isMobile && (<img src={logoMini} alt="Gracious moments logo" />)}
+          {!isMobile && (<img src={logoMedium} alt="Gracious moments logo" />)}
           <div className="icons">
             <img src={menuicon} alt="menu" className="menu" />
             <img src={searchicon} alt="search" className="search" />
@@ -44,51 +47,3 @@ const Header = () => {
 };
 
 export default Header;
-
-/*
-import React, { useState, useEffect } from 'react';
-import logo from '../assets/logos/Vector.png';
-import menuicon from '../assets/icons/Menu.png';
-import searchicon from '../assets/icons/Search.png';
-import './Header.css';
-
-const Header = () => {
-  const [screenSize, setScreenSize] = useState(window.innerWidth);
-  const isDesktop = screenSize >= 1025;
-  console.log('screenSize', screenSize)
-  console.log('isDesktop', isDesktop)
-
-  useEffect(() => {
-    const handleResize = () => setScreenSize(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return (
-    <div className="Header">
-      <img src={logo} alt="Gracious moments logo" />
-
-      {!isDesktop && (
-        <div className="icons">
-          <img src={menuicon} alt="menu" className="menu" />
-          <img src={searchicon} alt="search" className="search" />
-        </div>
-      )}
-
-      {isDesktop && (
-        <div className="navbar-text">
-          <div className="menu-text">
-            <h3>Classes</h3>
-            <h3>About</h3>
-            <h3>Contact</h3>
-          </div>
-          <h2 className="search-text">Search</h2>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Header;
-
-*/
