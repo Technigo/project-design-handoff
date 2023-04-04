6249041eeccf82dc8c0ec9cf13f813538787cb08
AnnikaSonnek <annika.sonnek@gmail.com>
2023-04-04 21:50:49 +0200

    fixed hamburgermenu and search-icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const HeaderWrapper = styled.header`
@@ -12,10 +12,69 @@ margin: 16px;
 const NavContainer = styled.div`
 display: flex; 
 flex-direction: row; 
-gap: 10px; 
+gap: 20px; 
 `
+const HamSearchIcon = styled.div`
+  display: block;
+  width: 24px;
+  height: 24px;
+  background-repeat: no-repeat;
+  background-position: center;
+  cursor: pointer;
+  color: white;
+`;
+
+const CloseIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  background-repeat: no-repeat;
+  background-position: center;
+  cursor: pointer;
+  color: white; 
+  display: flex; 
+  align-self: flex-end; 
+  padding: 16px;
+`;
+
+const OverlayMenu = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 60%;
+  height: 100%;
+  background-color: black;
+  z-index: 9999;
+  padding: 20px;
+  display: flex;
+  flex-direction: column; 
+  align-items: right;
+`;
+
+const NavigationLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  padding-top: 100px; 
+`;
+
+const MenuItem = styled.a`
+  display: block;
+  margin-bottom: 20px;
+  color: white;
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 20.16px;
+`;
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
   return (
     <HeaderWrapper>
       <svg width="35" height="48" viewBox="0 0 35 48" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
@@ -25,14 +84,36 @@ export const Header = () => {
       </svg>
 
       <NavContainer>
-        <p>Search-icon</p>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
+        <HamSearchIcon>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M21 21L16.65 16.65" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </HamSearchIcon>
+        {open ? (
+          <OverlayMenu>
+            <CloseIcon onClick={handleToggle} aria-label="close">
+              <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <line x1="1.18556" y1="18.4469" x2="17.6931" y2="1.93933" stroke="white" strokeWidth="3" />
+                <line y1="-1.5" x2="23.3452" y2="-1.5" transform="matrix(-0.707107 -0.707107 -0.707107 0.707107 16.5076 19.5076)" stroke="white" strokeWidth="3" />
+              </svg>
+            </CloseIcon>
+            <NavigationLinks>
+              <MenuItem href="#">Our gym</MenuItem>
+              <MenuItem href="#">Classes</MenuItem>
+              <MenuItem href="#">About us</MenuItem>
+              <MenuItem href="#">Contact</MenuItem>
+            </NavigationLinks>
+          </OverlayMenu>
+        ) : (
+          <HamSearchIcon onClick={handleToggle}>
+            <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4.125 16.5H28.875" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M4.125 8.25H28.875" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M4.125 24.75H28.875" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </HamSearchIcon>
+        )}
       </NavContainer>
     </HeaderWrapper>
   )
