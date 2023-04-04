9b078686715a769b673ca69fbe019d4dc9d474f9
EmiliaGA <emilia-lundmark@hotmail.com>
2023-04-04 21:52:14 +0200

    Benefits, added assets

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -1,41 +1,64 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import styled from 'styled-components'
-import yoga from 'yoga.png';
+import Yoga from 'Yoga.png';
 import Blob from 'Blob.png';
 
 const BenefitsWrapper = styled.div`
-    background-color: white;
     display: flex;
     flex-direction: column;
-
-    @media (min-width: 744px) {
-        width: 300px;
-    }
-
+    justify-content: center;
+    align-content: center;
+    
 `
 
 const Text = styled.div`
+    background-image: url(${Blob});
+    background-size: contain;
+    background-repeat: no-repeat;
     color: green;
-    
+    background-position: center;
+    margin: 20px;
+    height: 40em;
+    p {
+        text-align: center;
+    }
+
+    @media (min-width: 968px) {
+        height: 
+    }
 `
 const Heading = styled.h1`
     color: green;
+    text-align: center;
+`
+const ImageDiv = styled.div`
+    .yogaPic {
+        width: 100%;
+        height: auto
+    }
+
+    .yogaVector {
+        
+    }
 `
 
 export const Benefits = () => {
   return (
     <BenefitsWrapper>
-      <Heading>Benefits of Yoga</Heading>
-      <Text>There is a wide range of yoga and meditation classes here.
+      <Text>
+        <Heading>Benefits of Yoga</Heading>
+        <p>There is a wide range of yoga and meditation classes here.
             Here you want to create community and there is always good
             tea and someone you can talk to if you feel like it after class.
             You are warmly welcome regardless of experience.
             It also has many psychological and physical effects, including lowering
             stress levels and blood pressure while improving concentration and memory.
+        </p>
       </Text>
-      <img src={Blob} />
-      <img src={yoga} />
+      <ImageDiv>
+        <img className="yogaPic" src={Yoga} />
+      </ImageDiv>
     </BenefitsWrapper>
   )
 }
