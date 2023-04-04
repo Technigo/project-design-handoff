0a827588178c65dd784eaa972ae1f7e1be45ae37
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-04-04 21:49:32 +0200

    started on pitch section

--- src/components/Pitch.js
+++ src/components/Pitch.js
@@ -3,10 +3,8 @@ import React from 'react';
 export const Pitch = () => {
   return (
     <div className="pitch-container">
-      <h2> How we get you in shape </h2>
-      <div className="card-container">
-        picture+text
-      </div>
+      <h2 className="pitch-title"> How we get you in shape: </h2>
+      <div className="pitch-cards" />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import './css/NavBar.css';
 import './css/Hero.css';
-import './css/Methods.css'
+import './css/Methods.css';
+import './css/Pitch.css';
 import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'))
