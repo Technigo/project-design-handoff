58cf768d05ceb1179f6f98cdc482549c1d91b7aa
Sammy <sammyolsson@gmail.com>
2023-04-04 22:00:25 +0200

    Started with navigationbar and hero components, going desktop first for some reason

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { NavigationBar } from './components/NavigationBar/NavigationBar';
+import { Hero } from './components/Hero/Hero';
 
 export const App = () => {
   return (
     <div>
+      <NavigationBar />
+      <Hero />
       Find me in src/app.js!
     </div>
   )
