af5c25c3ade120745df9a7cb6bacf9178d6da6d3
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-04 20:11:33 +0200

    add class component

---
+++ src/components/Classes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Classes = () => {
+  return (
+    <div className="class-container">
+      <div className="classes">
+        <h3>Classes</h3>
+        <p>Beginner</p>
+        <p>Intermediate</p>
+        <p>Expert</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default Classes;
