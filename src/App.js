277e5cfd1b2558c8c090c16a650185c1a1fe2f60
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-04-04 15:49:35 +0200

    Fixing navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Navbar from 'components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 
 export const App = () => {
   return (
-    <div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/products" />
+        <Route path="/contact" />
+        <Route path="/about" />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,15 @@
 import React from 'react';
+import { NavbarContainer } from '../styles/Navbar.style';
 
-/* Section 1: HERO */
-/* Logo */
-/* Navbar */
-/* Background picture with a "blob" with text on it */
-/* Two buttons - CTA: "Get started" and "Download App" */
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      <ul>
+        <li>Hello</li>
+        <li>You</li>
+      </ul>
+    </NavbarContainer>
+  )
+}
 
-/* Section 2 - THE BENEFITS */
-/* Section with list */
-/* Image of a Paw in the right side corner, tilted? */
-
-/* Section 3 - SPEAK TO THE GOALS (FIND ACTIVITIES) */
-/* CTA: "Get started"-button */
-/* 6 different "blobs" with activities and different style */
-
-/* Section 4 - HOW TO ACHIEVE (WORKOUT WHERE YOU ARE) */
-/* Download App-button */
-/* List with USPs and hover-over-effect */
-
-/* Section 5 - SOCIAL PROOF */
-/* Some text */
-/* Find-out-more-button */
-/* 3 small statistic "blobs" */
-/* Area with quote in it */
-
-/* Section 6 - FOOTER */
-/* Two buttons - Get started and Download App */
-
+export default Navbar;
