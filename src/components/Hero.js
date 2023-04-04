0edc2d543e7664d5a744072197e8a4985f05d03c
tess.aquilon <tess.aquilon@gmail.com>
2023-04-04 15:59:51 +0200

    title

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,20 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable max-len */
 import React from 'react';
+import styled from 'styled-components'
 
 export const Hero = () => {
+  const Title = styled.h1`
+    font-family: 'Libre Franklin';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 48px;
+    line-height: 58px;
+
+    color: #025323;`
   return (
     <>
-      <h1>Breath in breath out</h1>
+      <Title>Breath in breath out</Title>
       <p>Yogistudio offers an overall concept in self-healing with a focus on calm and therapeutic forms of yoga.</p>
       <label>
         <btn type="button" id="bookAClass">Book a class</btn>
