7ffa1f0887a9b8c00570506d9615cb3b5504cb6e
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-04 21:08:12 +0200

    css fix to img

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import BeginnerImg from '../assets/images/Beginner.png'
+// import IntermediateImg from '../assets/images/Intermediate.png'
 import './Classes.css'
 
 const Classes = () => {
   return (
     <div className="class-container">
       <div className="classes">
-        <h3>Classes</h3>
+        <h3>Our Classes</h3>
+        <img src={BeginnerImg} alt="beginner-img" />
         <p>Beginner</p>
+        {/* <img src={IntermediateImg} alt="intermediate-img" /> */}
         <p>Intermediate</p>
         <p>Expert</p>
 
