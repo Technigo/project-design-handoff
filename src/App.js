af96eb10fcf5942bd5abfd7b2439987aaf745c7c
GG_Cugs <giorgio.cugusi@gmail.com>
2023-04-04 18:33:40 +0200

    first steps
    header and 3 sections done in mobile view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Testimonials } from 'components/Testimonials';
+import { Header } from './components/Header';
+import { Benefits } from './components/Benefits';
+import { CTAsection } from './components/CTAsection';
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Header />
+      <Benefits />
+      <CTAsection />
+      <Testimonials />
+      <CTAsection />
     </div>
   )
 }
---
+++ src/components/CTAsection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from './Button';
+import './CTAsection.css'
+
+export const CTAsection = () => {
+  return (
+    <div className="cta-background">
+      <Button>Start your 7-day free trial</Button>
+    </div>
+  );
+}
