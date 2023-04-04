b1a3d34aedc27a43d25280b01f859d1a79e39b3f
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-04-04 19:58:43 +0200

    hero-section almost done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,14 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 export const Hero = () => {
   return (
     <div className="hero-container">
-      <h1 className="title-text"> Becoming a fit parent</h1>
-      <button aria-label="read more" className="button" type="button" />
+      <div className="hero-text-and-button">
+        <h1 className="title-text"> Becoming a fit parent</h1>
+        <h4> Say Goodbye to Excuses and Hello to Results with Our Online Fitness Program for Busy Parents.</h4>
+        <button aria-label="read more" className="button" type="button">Start trial</button>
+      </div>
     </div>
   )
 }
