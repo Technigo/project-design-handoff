7200d76d9e84f72b6c82bcd632fc7d389c41ed59
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-04-04 16:24:29 +0200

    Planned out the basic structure and created + mounted  all section components. Also completed the TextBox components, incl. responsiveness

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+/* eslint-disable linebreak-style */
 import React from 'react'
+import { Section } from './components/Section'
+import { TextBox } from './components/TextBox'
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Section image>Top section: Mount header and into/sign-up div here.</Section>
+      <Section brown removeOnMobile>
+        <TextBox
+          heading="What is Power Yoga?"
+          text="Power yoga is a fast-paced style of yoga that's focused on building strength and endurance. Whilst practising this form of yoga, you will feel the tension and toxins leaving your body and your posture and back pain getting better." />
+      </Section>
+      <Section white>Classes section</Section>
+      <Section brown removeOnMobile>
+        <TextBox
+          heading="Santulan Power Yoga Studio"
+          text="Put simply, Yoga transformed our lives and we want to share that magic with you. We created Santulan to empower your journey to personal growth and well-being. Our classes weave ancient yogic philosophy into 21st century life, and everyone’s welcome, from absolute beginners to advanced practitioners. We passionately believe that Power Yoga is for everyone, and no matter your age or flexibility, our expert teachers will ensure you leave each class stronger in mind and body." />
+      </Section>
+      <Section white>Find Us: mount location infobox here</Section>
+      <Section blue halfWidth>Contact section</Section>
+      <Section white halfWidth>Sign up section</Section>
+      <Section brown>Footer section</Section>
     </div>
   )
 }
---
+++ src/components/Section.js
@@ -0,0 +1,50 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-nested-ternary */
+import styled, { css } from 'styled-components'
+
+export const Section = styled.section`
+  display: block;
+  border: pink dotted 3px;
+  color: blue;
+  /*background-color: ${(colour) => (colour.white ? 'white' : colour.blue ? 'blue' : 'brown')}  
+  This does't seem to allow for hex colors*/
+  background-color: white;
+  
+  /* Background-image for the top-section */
+  ${(background) => background.image && css`
+    background: #2E160E url('https://images.pexels.com/photos/4325484/pexels-photo-4325484.jpeg');
+    background-size: cover;
+    transform: scaleX(-1);
+    color: red;
+    height: 982px;
+  `}
+  /* Background-colours for the different sections */
+  ${(background) => background.brown && css`
+    background-color: #2E160E;
+  `}
+  ${(background) => background.white && css`
+    background-color: #FFFFFF
+;
+  `}
+  ${(background) => background.blue && css`
+    background-color: #66A1AE;
+  `}
+
+  /* Media Queries for responsivenes */
+
+  /* Removes certain sections on mobile */
+  @media (max-width: 453px) {
+    ${(remove) => remove.removeOnMobile && css`
+      display: none;
+    `}
+  }
+  /* Changes layout on larger screens/desktop */
+  @media (min-width: 686px) {
+    border: none;
+    ${(size) => size.halfWidth && css`
+      width: 50%;
+      display: inline-block; 
+    `}
+  }
+
+`
