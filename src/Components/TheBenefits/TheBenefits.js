327c6e37ef0df95da8231bf571ef10017e69ed48
Andreas Axelsson <pererikandreas@gmail.com>
2023-04-05 00:17:31 +0200

    added css

--- src/Components/TheBenefits/TheBenefits.js
+++ src/Components/TheBenefits/TheBenefits.js
@@ -2,13 +2,41 @@
 
 import React from 'react';
 import './TheBenefits.css';
+import checkmark from './Vector26.svg';
 
 // /////////////// COMPONENT //////////////////////// //
 
 export const TheBenefits = () => {
   return (
     <div className="thebenefits-box">
-      <p className="footer-text">THE BENEFITS</p>
+      <h2>BUILD HEALTHY HABITS</h2>
+      <h2>FOR YOU AND YOUR DOG</h2>
+      <ul>
+        <div className="check-text">
+          <img className="checkmark" src={checkmark} alt="checkmark" />
+          <p>Work your mind, body, have fun and develop amazing skills together. </p>
+        </div>
+        <div className="check-text">
+          <img className="checkmark" src={checkmark} alt="checkmark" />
+          <p>Get the most out of your time together every day.</p>
+        </div>
+        <div className="check-text">
+          <img className="checkmark" src={checkmark} alt="checkmark" />
+          <p>Levels from beginner to advanced dog owner/puppy to old dog.</p>
+        </div>
+        <div className="check-text">
+          <img className="checkmark" src={checkmark} alt="checkmark" />
+          <p>Online workouts and programs.</p>
+        </div>
+        <div className="check-text">
+          <img className="checkmark" src={checkmark} alt="checkmark" />
+          <p>Research based training.</p>
+        </div>
+        <div className="check-text">
+          <img className="checkmark" src={checkmark} alt="checkmark" />
+          <p>Experienced coaches.</p>
+        </div>
+      </ul>
     </div>
   )
 }
