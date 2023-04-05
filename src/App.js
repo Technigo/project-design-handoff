a4d516fc5395b26018905a07b7ad120816b54ce6
Peki <112956568+akipeki@users.noreply.github.com>
2023-04-05 13:05:28 +0300

    update dump header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './global.css';
+import Menu from 'components/Menu'
+import Footer from 'components/Footer'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <div>
+        <Menu />
+      </div>
+      <Footer />
+    </>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="text-container">
+        <p>Contact us:</p>
+        <p>acrogym@sportsorg.se</p>
+        <p>mob: 078 652 45 32</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
