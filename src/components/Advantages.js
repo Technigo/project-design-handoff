4a55469ba81bca7ef6be81518103caff1d8c003e
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-04-05 12:55:19 +0200

    img on the list

--- src/components/Advantages.js
+++ src/components/Advantages.js
@@ -5,10 +5,39 @@ import './Advantages.css';
 const Advantages = () => {
   return (
     <div className="advantages-container">
-      <h3>Personal Coach support and flexibility of Online Training</h3>
-      <h3>Your preferred workout and diet plan</h3>
-      {/* Add the rest of your component content */}
+      <div className="coach">
+        <h3>Personal Coach support and flexibility of Online Training</h3>
+        <h4>Your preferred workout and diet plan</h4>
+        <ul className="custom-list">
+          <li>Monitor your progression and technique </li>
+          <li>Keeps you accountable and motivated</li>
+        </ul>
+
+        <h4>Online Workout</h4>
+        <ul className="custom-list">
+          <li>Save time and resources </li>
+          <li>Fit the sessions into your schedule</li>
+          <li>Never boring</li>
+        </ul>
+      </div>
+
+      <div className="diet">
+        <h3>Your preferred workout and diet plan</h3>
+        <ul className="custom-list">
+          <li>We offer various type of workout  </li>
+          <li>Trainings are conducted by certified
+             coaches and tailored according to your
+              personal needs
+          </li>
+        </ul>
+        <h4>Diet plan</h4>
+        <ul className="custom-list">
+          <li>We offer different healthy diets</li>
+          <li>Diet plans are customised according to your health conditions and background </li>
+        </ul>
+      </div>
     </div>
+
   );
 };
 
