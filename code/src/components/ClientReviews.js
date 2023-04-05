4567162d285ebf3750d4f3457c36cb6fa8bbb3ae
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-05 13:13:00 +0200

    adding client review cont

--- code/src/components/ClientReviews.js
+++ code/src/components/ClientReviews.js
@@ -1,41 +1,43 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable max-len */
 
 import React from 'react';
 import Slider from 'react-slick';
+import './ClientReview.css';
 
-const SwipeableCarouselClientReviews = () => {
+const ClientReviewsCarousel = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
   };
 
   return (
-    <>
+    <div className="client-review-container">
       <h1>What our Clients say about their journey!</h1>
       <Slider {...settings}>
-        <div>
+        <div className="card">
           <p>“Arranging the free trial and signing up for a package afterward was quick and easy. My trainer Lola is AMAZING! I absolutely hate exercise and have not been able to stick to an exercise routine since school. But Lola makes my workouts fun and keeps me sessions!”</p>
+          <img src="../assets/Liana.jpg" alt="picture of Liana" />
           <h3>Liana Kelly</h3>
         </div>
-        <div>
+        <div className="card">
           <p>“My personal coach, Sara, makes my workouts fun and keeps me motivated by adjusting my workouts for my specific needs. Now that I am working with her, I look forward to my workout sessions!”</p>
+          <img src="../assets/Nils.png" alt="picture of Nils" />
           <h3>Nils Beck</h3>
         </div>
-        <div>
+        <div className="card">
           <p>“Thanks to my coach, I could finally stick to the lifestyle always looking for. Making customized eating diet and training based on my routine helps me to keep motivated  and easy to follow!”</p>
+          <img src="../assets/Sara.png" alt="picture of Sara" />
           <h3>Sara Jones</h3>
         </div>
-        <div>
-          <h3>Card 4</h3>
-          <p>Vivamus malesuada posuere justo, sed egestas lectus bibendum et.</p>
-        </div>
       </Slider>
-    </>
+    </div>
   );
 };
 
-export default SwipeableCarouselClientReviews;
+export default ClientReviewsCarousel;
