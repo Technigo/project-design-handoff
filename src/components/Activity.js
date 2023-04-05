eb317d0e8675a4e411ad96a8bac25853d346bb4f
fannystenberg <ffannystenberg@gmail.com>
2023-04-06 00:17:08 +0200

    added carousel for activity section

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import Slider from 'react-slick';
 import { Tagline, Headline5, Headline4, Body } from 'styling/Text';
 
 export const TextCard = ({ headlineTitle, bodyText }) => {
@@ -10,7 +12,41 @@ export const TextCard = ({ headlineTitle, bodyText }) => {
   )
 };
 
+export const Bullets = () => {
+  return (
+    <>
+      <div className="TextCards1">
+        <TextCard
+          headlineTitle="Milestones"
+          bodyText="Don't have any personal goals yet? Challenge yourself by completing our
+          pre-made milestones that definitely will keep you motivated" />
+        <TextCard
+          headlineTitle="Stats"
+          bodyText="View your general stats and keep track of your achievments" />
+      </div>
+      <div className="TextCards2">
+        <TextCard
+          headlineTitle="Schedule"
+          bodyText="Schedule and monitor all your workouts in the calender and set up
+          a personal reminder to keep you going" />
+        <TextCard
+          headlineTitle="Personal"
+          bodyText="Track any personal fitness activities you accomplish by adding them to your calender" />
+      </div>
+    </>
+  )
+};
+
 const Activity = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true
+  };
+
   return (
     <section className="sectionActivity">
       <div className="introActivity">
@@ -18,22 +54,25 @@ const Activity = () => {
         <Headline5>Track your activity & celebrate your progress</Headline5>
       </div>
       <img alt="img" className="phoneImg" src="../images/mobile-white.png" />
-      <div className="scrollWrapper">
-        <TextCard
-          headlineTitle="Milestones"
-          bodyText="Don't have any personal goals yet? Challenge yourself by completing our
+      <div className="activitySlider">
+        <Slider {...settings}>
+          <TextCard
+            headlineTitle="Milestones"
+            bodyText="Don't have any personal goals yet? Challenge yourself by completing our
           pre-made milestones that definitely will keep you motivated" />
-        <TextCard
-          headlineTitle="Schedule"
-          bodyText="Schedule and monitor all your workouts in the calender and set up
+          <TextCard
+            headlineTitle="Schedule"
+            bodyText="Schedule and monitor all your workouts in the calender and set up
           a personal reminder to keep you going" />
-        <TextCard
-          headlineTitle="Stats"
-          bodyText="View your general stats and keep track of your achievments" />
-        <TextCard
-          headlineTitle="Personal"
-          bodyText="Track any personal fitness activities you accomplish by adding them to your calender" />
+          <TextCard
+            headlineTitle="Stats"
+            bodyText="View your general stats and keep track of your achievments" />
+          <TextCard
+            headlineTitle="Personal"
+            bodyText="Track any personal fitness activities you accomplish by adding them to your calender" />
+        </Slider>
       </div>
+      <Bullets />
     </section>
   )
 };
