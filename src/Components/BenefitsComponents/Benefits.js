aecb9ae23b1f6d8eab314303fc7274b5f5c74424
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-04-05 12:19:38 +0200

    started on benefits sec

--- src/Components/BenefitsComponents/Benefits.js
+++ src/Components/BenefitsComponents/Benefits.js
@@ -4,12 +4,12 @@ const Benefits = () => {
   return (
     <section className="benefits-section">
       <h3 className="benefits-header">BENEFITS OF FIT2DERBY</h3>
-      <div>
+      <div className="benefits-div">
         <p>sdjfnsödnfsdlanfkldsnfkldsnfkldsnxfklns
           sdlzfnödsklnfklsdnfklnsdkln ldsnflkdsanfklnsdaklnfkln
         </p>
-        <h4>EXAMPLES OF EXERCISES</h4>
-        <ul>
+        <h4 className="examples-header">EXAMPLES OF EXERCISES</h4>
+        <ul className="examples-list">
           <li>ösnfjgn</li>
           <li>dfgdfgdfg</li>
           <li>dflkgndföklng</li>
