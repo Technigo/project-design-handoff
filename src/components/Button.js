d857fffd2ab03619c0ad63752081c46d406786f4
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-04-05 17:24:23 +0200

    Completed the location component/section and almost completed the sign-up form. Only thing missing from that is styling the radio buttons, which proved tricky.

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  width: 100%;
+  background-color: #FF9F0A;
+  text-align: center;
+  color: #000000; 
+  font-family: 'Outfit', sans-serif;
+  font-weight: 600;
+  font-size: 24px;
+  padding: 10px;
+  cursor: pointer;
+  border: none;
+  border-radius: 20px;
+`
--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,6 @@ import styled, { css } from 'styled-components'
 
 export const Section = styled.section`
   display: block;
-  border: pink dotted 3px;
   color: blue;
   /*background-color: ${(colour) => (colour.white ? 'white' : colour.blue ? 'blue' : 'brown')}  
   This does't seem to allow for hex colors*/
@@ -12,9 +11,11 @@ export const Section = styled.section`
   
   /* Background-image for the top-section */
   ${(background) => background.image && css`
-    background: #2E160E url('https://images.pexels.com/photos/4325484/pexels-photo-4325484.jpeg');
+    /*background: #2E160E url('https://images.pexels.com/photos/4325484/pexels-photo-4325484.jpeg');*/
+    background: #2E160E url('../images/hero-image.png');
     background-size: cover;
-    transform: scaleX(-1);
+    background-position: center;
+    /*transform: scaleX(-1);*/
     color: red;
     height: 982px;
   `}
