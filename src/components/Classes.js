0b63bb67fbd79e36d836f591e7d057f80d8117f0
Fiona Klacar <fiona.klacar@gmail.com>
2023-04-05 21:23:51 +0200

    styled the Classes section for mobile

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -5,17 +5,19 @@ const Classes = () => {
   return (
     <div className="classesWrapper">
       <img className="ribbonOnBarreImage" src="../images/PictureFrameHalfPill.svg" alt="Ribbon on barre" />
-      <img className="vectorWhite" src="../images/Vector.svg" alt="Vector" />
-      <h2>Come Sweat It Out at the Barre</h2>
-      {/* // eslint-disable-next-line max-len */}
-      <p>Our Barre Classes are full-body workouts where we&apos;ll pulse,
-         plie, and sweat our way through a series of exercises.
-      </p>
-      <p>No dance experience required!</p>
-      <button type="button" className="ourClassesButton">Our Classes</button>
-
+      <div className="infoWrapper">
+        <img className="vectorImage" src="../images/Vector.svg" alt="Vector" />
+        <div className="infoSection">
+          <h2>Come Sweat It Out at the Barre</h2>
+          {/* // eslint-disable-next-line max-len */}
+          <p>Our Barre Classes are full-body workouts where we&apos;ll pulse,
+                plie, and sweat our way through a series of exercises.
+          </p>
+          <p className="boldText">No dance experience required!</p>
+          <button type="button" className="ourClassesButton">Our Classes</button>
+        </div>
+      </div>
     </div>
-
   )
 }
 
