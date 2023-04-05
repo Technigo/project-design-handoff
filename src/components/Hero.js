68017eca9d286c32f73d579295f864472ab778e7
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-04-05 15:59:42 +0200

    created styled reusable button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
+import { StyledTrialBtn } from './TrialBtn';
 
 export const Hero = () => {
   return (
@@ -7,7 +8,7 @@ export const Hero = () => {
       <div className="hero-text-and-button">
         <h1 className="title-text"> Becoming a fit parent</h1>
         <h4> Say Goodbye to Excuses and Hello to Results with Our Online Fitness Program for Busy Parents.</h4>
-        <button aria-label="read more" className="button" type="button">Start trial</button>
+        <StyledTrialBtn> Start trial </StyledTrialBtn>
       </div>
     </div>
   )
---
+++ src/components/TrialBtn.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const StyledTrialBtn = styled.button`
+  width: 390px; 
+  height: 84px; 
+  background: #DBFE62;
+  border-radius: 47px;
+  align-self: center; 
+  font-family: 'Poppins'; 
+  font-size: 24px; 
+  font-weight: 600; 
+  border: none;
+
+  @media (min-width:667px) and (max-width:1024px){
+    width: 334px; 
+  }
+
+  @media (max-width: 667px) {
+    width: 325px; 
+    height: 68px; 
+  }
+  
+  `
+
+// export const TrialBtn = () => {
+//   return (
+//     // <button aria-label="read more" className="button" type="button">Start trial</button>
+//     <StyledTrialBtn>Start Trial</StyledTrialBtn>
+//   )
+// }
+
