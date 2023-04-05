7e18f4a3790bc775552ede4a80da98714f217e55
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-05 17:43:34 +0200

    adding form text

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SilkTop from '../silk/SilkTop'
 import SilkBottom from '../silk/SilkBottom'
 import BeginnerImg from '../assets/images/Beginner.png'
+
 // import IntermediateImg from '../assets/images/Intermediate.png'
 import './Classes.css'
 
@@ -13,9 +14,8 @@ const Classes = ({ screenSize }) => {
         <div className="classes">
           <h3>Our Classes</h3>
           <img src={BeginnerImg} alt="beginner-img" />
-          See more
+          <p>See more</p>
           <p>Beginner</p>
-
           <p>Intermediate</p>
           <p>Expert</p>
         </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,7 +5,34 @@ const SignUp = () => {
   return (
     <div className="signup-container">
       <div className="signupform">
-        <p>Sign up form here</p>
+        <p>Form for sign up</p>
+        <form>
+          <input type="text" placeholder="First Lastname" />
+          <input type="text" placeholder="example@email.com" />
+          <input type="text" placeholder="+46 123 11 22 33" />
+          <select>
+            <option value="" selected>Classes</option>
+            <option value="Beginner class">Beginner class</option>
+            <option value="Intermediate class">Intermediate class</option>
+            <option value="Expert class">Expert class</option>
+          </select>
+          <h3 className="radio-text">Do you have any prior experience with aerial silks?</h3>
+          <div className="radio-div">
+            <input type="radio" />
+            <p>Yes</p>
+            <input type="radio" />
+            <p>No</p>
+            <input type="radio" />
+            <p>I do not know</p>
+          </div>
+          <h3 className="radio-text">Do you want us to contact you for any other inquiries?</h3>
+          <div className="radio-div">
+            <input type="radio" />
+            <p>Yes</p>
+            <input type="radio" />
+            <p>No</p>
+          </div>
+        </form>
       </div>
     </div>
   );
