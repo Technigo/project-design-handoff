aa1461485ce4e9889bb1093d1f53172b39dd614b
tess.aquilon <tess.aquilon@gmail.com>
2023-04-05 19:04:59 +0200

    added hero img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable max-len */
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import heroImage from './heroImage.jpg';
 
 export const Hero = () => {
   const Heading = styled.h1`
@@ -11,15 +12,17 @@ export const Hero = () => {
     font-weight: 600;
     font-size: 48px;
     line-height: 58px;
+    position: absolute;
 
     color: #025323;`
   return (
-    <>
+    <div className="hero-container">
+      <img className="hero-img" src={heroImage} alt="Person doing yoga" />
       <Heading>Breath in breath out</Heading>
       <p>Yogistudio offers an overall concept in self-healing with a focus on calm and therapeutic forms of yoga.</p>
       <label>
         <btn type="button" id="bookAClass">Book a class</btn>
       </label>
-    </>
+    </div>
   )
 }
