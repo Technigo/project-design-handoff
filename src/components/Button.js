ea291d7bb4a0a56573dca1f61a2dbf1f0981c11a
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-05 18:59:42 +0200

    Add button

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  color: #FF8F3D;
+  border: solid #FF8F3D;
+  border-radius: 6px;
+  font-size: 0.9rem;
+  font-weight: bold;
+  padding: 0.3rem 1.25rem;
+  cursor: pointer;
+`
+
+export default Button;
