f7ad04106ae1f8ab03754e16ddc28e2e19bb06fa
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-05 14:03:54 +0200

    added component and css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './sections/Header'
 import Hero from './sections/Hero'
 import About from './components/About'
 import Classes from './components/Classes'
+import Footer from './sections/Footer'
 
 import './index.css'
 
-export const App = () => {
+const App = () => {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div>
-      <Header />
+    <div className="App">
+      <Header screenSize={screenSize} />
       <Hero />
       <About />
-      <Classes />
+      <Classes screenSize={screenSize} />
+      <Footer screenSize={screenSize} />
     </div>
   )
 }
+export default App;
