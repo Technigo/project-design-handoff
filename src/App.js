3657d9d576c848a0c672668a2b99b0b6f520bf4f
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-05 19:00:25 +0200

    Add header to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
+// import styled from 'styled-components'
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Header />
     </div>
   )
 }
