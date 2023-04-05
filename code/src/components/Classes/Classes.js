4fd15ba9502f3c765c922bd8ce1369d6b5dec6ff
Frid Matilda <matilda.x.frid@scania.com>
2023-04-05 20:48:09 +0200

    classes styling fixes

--- code/src/components/Classes/Classes.js
+++ code/src/components/Classes/Classes.js
@@ -3,6 +3,8 @@ import SilkTop from './Silk/SilkTop.js';
 import SilkBottom from './Silk/SilkBottom.js';
 import Class from './Class.js';
 import BeginnerImg from '../../assets/images/Beginner.png';
+import IntermediateImg from '../../assets/images/Intermediate.png';
+import ExpertImg from '../../assets/images/Expert.png';
 
 import './Classes.css';
 
@@ -31,8 +33,8 @@ const Classes = ({ screenSize }) => {
         <h3>Our Classes</h3>
         <div className="classes">
           <Class screenSize={screenSize} level="Beginner" img={BeginnerImg} description="Beginner classes for aerial silks teach you the foundational moves and techniques, and is led by experienced instructors to guide you. No prior experience is necessary." />
-          <Class screenSize={screenSize} level="Intermediate" img={BeginnerImg} description="Intermediate classes build on foundational moves and introduce complex techniques, led by experienced instructors. They improve strength, flexibility, and coordination." />
-          <Class screenSize={screenSize} level="Expert" img={BeginnerImg} description="Expert classes are for the experienced aerialists, which offer a challenging workout with advanced techniques. Led by skilled instructors to help guide you to refine your skills." />
+          <Class screenSize={screenSize} level="Intermediate" img={IntermediateImg} description="Intermediate classes build on foundational moves and introduce complex techniques, led by experienced instructors. They improve strength, flexibility, and coordination." />
+          <Class screenSize={screenSize} level="Expert" img={ExpertImg} description="Expert classes are for the experienced aerialists, which offer a challenging workout with advanced techniques. Led by skilled instructors to help guide you to refine your skills." />
         </div>
       </div>
       <SilkBottom screenSize={screenSize} />
