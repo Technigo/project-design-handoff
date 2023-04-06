4692824053dff0aafa3e923909dc7accde655f10
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-06 10:34:04 +0200

    more css

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import { CgMenu } from 'react-icons/cg'
+import React, { useState } from 'react'
+import { CgMenu, CgClose } from 'react-icons/cg'
 import NavLinks from './NavLinks'
 import Classes from './css/classes.module.css'
 
 const Mobile = () => {
+  const [open, setOpen] = useState(false)
+
+  const hamburgerIcon = <CgMenu
+    className={Classes.Hamburger}
+    size="40px"
+    color="white"
+    onClick={() => setOpen(!open)} />
+
+  const closedIcon = <CgClose
+    className={Classes.Hamburger}
+    size="40px"
+    color="white"
+    onClick={() => setOpen(!open)} />
+
   return (
     <nav className={Classes.MobileNavigation}>
-      <CgMenu className={Classes.Hamburger} size="40px" color="white" />
-      <NavLinks />
+      {open ? closedIcon : hamburgerIcon}
+      {open && <NavLinks />}
     </nav>
   )
 }
