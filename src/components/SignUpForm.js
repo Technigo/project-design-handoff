f762673091cda405b7cd8e4530ce7ee1d7f33d14
Peki <112956568+akipeki@users.noreply.github.com>
2023-04-06 15:30:39 +0300

    footer fixed

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -41,59 +41,49 @@ const SignUpForm = () => {
       <section className="sign-up-form">
         <div className="sign-up-list">
           <form onSubmit={handleSubmit}>
-            <label htmlFor="fname">
-              First name:
-              <br />
-              <input
-                type="text"
-                id="fname"
-                name="fname"
-                value={formData.fname}
-                onChange={handleChange}
-                required />
-            </label>
+            <label htmlFor="fname">First name:</label>
             <br />
-            <label htmlFor="lname">
-              Last name:
-              <br />
-              <input
-                type="text"
-                id="lname"
-                name="lname"
-                value={formData.lname}
-                onChange={handleChange}
-                required />
-            </label>
+            <input
+              type="text"
+              id="fname"
+              name="fname"
+              value={formData.fname}
+              onChange={handleChange}
+              required /><br />
+            <label htmlFor="lname">Last name:</label>
             <br />
-            <label htmlFor="email">
-              E-mail:
-              <br />
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required />
-            </label>
+            <input
+              type="text"
+              id="lname"
+              name="lname"
+              value={formData.lname}
+              onChange={handleChange}
+              required /><br />
+            <label htmlFor="email">E-mail:</label>
             <br />
-            <label htmlFor="newsletter-checkbox">
-              Subscribe to Newsletter
-              <input
-                type="checkbox"
-                id="newsletter-checkbox"
-                name="checkbox"
-                checked={formData.checkbox}
-                onChange={handleChange}
-                value="subscribe" />
-            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required />
+            <br />
+            <label htmlFor="newsletter-checkbox">Subscribe to Newsletter</label>
+            <input
+              type="checkbox"
+              id="newsletter-checkbox"
+              name="checkbox"
+              checked={formData.checkbox}
+              onChange={handleChange}
+              value="subscribe" />
             <br />
             <input id="submitbutton" type="submit" value="Submit" />
           </form>
         </div>
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default SignUpForm;
+export default SignUpForm
