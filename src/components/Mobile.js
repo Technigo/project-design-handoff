0d378d59bd9827e8b441d0fb0aa9dfdf098865d5
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-06 10:01:07 +0200

    morecss

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { CgMenu } from 'react-icons/cg'
 import NavLinks from './NavLinks'
 import Classes from './css/classes.module.css'
 
 const Mobile = () => {
   return (
     <nav className={Classes.MobileNavigation}>
+      <CgMenu className={Classes.Hamburger} size="40px" color="white" />
       <NavLinks />
     </nav>
   )
