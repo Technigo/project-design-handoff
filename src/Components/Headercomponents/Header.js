fae6046f7b5f0ab7faa89a54bb801877e76c23e6
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-04-06 16:18:54 +0200

    inserted jammer

--- src/Components/Headercomponents/Header.js
+++ src/Components/Headercomponents/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Jammer from 'Images/Jammer.svg';
 import Navbar from './Navbar';
 
 const Header = () => {
@@ -7,9 +8,8 @@ const Header = () => {
       <Navbar />
       <h1 className="hero-header">UP YOUR GAME WITH FIT2DERBY!</h1>
       <h2 className="sub-header">Customised online off skate programs for roller derby players</h2>
-      <p className="body-copy">djkfnsdjfndslnfsdlnfäd <span>sdnlasödln fkdslnfkn</span>
-       sdfökdslfasnf <span>sdnlasödln fkdslnfkn</span> lsdköfngaödlsgn
-      </p>
+      <p className="body-copy">Choose between our <span>individual</span> sign-up and <span>Team sign-up</span> with exclusive access to our <span>Multiplayer Platform</span></p>
+      <img className="jammer" src={Jammer} alt="jammer" />
       <div className="button-container">
         <button className="signup-button" type="button">Individual signup</button>
         <button className="signup-button" type="button">Join with your team</button>
