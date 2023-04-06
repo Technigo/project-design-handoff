ab844c225d9d97195d923be7dfd7223973489e23
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-04-06 16:38:17 +0200

    styling details

--- src/components/Coaches.js
+++ src/components/Coaches.js
@@ -17,23 +17,23 @@ const Coaches = () => {
       <div className="slider-coach">
         <Slider {...settings}>
           <div className="cards-coach">
-            <img className="coach-image" src={`${process.env.PUBLIC_URL}/img/Coach1.png`} alt="Coach1" />
+            <img className="coach-image1" src={`${process.env.PUBLIC_URL}/img/Coach1.png`} alt="Coach1" />
           </div>
 
           <div className="cards-coach">
-            <img className="coach-image" src={`${process.env.PUBLIC_URL}/img/Coach2.png`} alt="Coach2" />
+            <img className="coach-image2" src={`${process.env.PUBLIC_URL}/img/Coach2.png`} alt="Coach2" />
           </div>
 
           <div className="cards-coach">
-            <img className="coach-image" src={`${process.env.PUBLIC_URL}/img/Coach3.png`} alt="Coach3" />
+            <img className="coach-image3" src={`${process.env.PUBLIC_URL}/img/Coach3.png`} alt="Coach3" />
           </div>
 
           <div className="cards-coach">
-            <img className="coach-image" src={`${process.env.PUBLIC_URL}/img/Coach4.png`} alt="Coach4" />
+            <img className="coach-image4" src={`${process.env.PUBLIC_URL}/img/Coach4.png`} alt="Coach4" />
           </div>
 
           <div className="cards-coach">
-            <img className="coach-image" src={`${process.env.PUBLIC_URL}/img/Coach5.png`} alt="Coach5" />
+            <img className="coach-image5" src={`${process.env.PUBLIC_URL}/img/Coach5.png`} alt="Coach5" />
           </div>
 
         </Slider>
