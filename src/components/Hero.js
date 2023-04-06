6591eaf87db9c98c01b31e0262036f530a467311
tess.aquilon <tess.aquilon@gmail.com>
2023-04-06 17:35:28 +0200

    mobile adjustments

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,17 +12,48 @@ export const Hero = () => {
     font-weight: 600;
     font-size: 48px;
     line-height: 58px;
+    color: #025323;
     position: absolute;
-
-    color: #025323;`
+    top: 82px;
+    left: 23px;
+    right: 53px;
+    width: 238px;`
+  const Text = styled.p`
+    font-family: 'Libre Franklin';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 22px;
+    line-height: 37px;
+    color: #025323;
+    position: absolute;
+    top: 221px;
+    left: 23px;
+    right: 53px;
+    min-width: 263px;
+    max-width: 280px;
+    `
+  const Button = styled.button`
+  font-family: 'Libre Franklin';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 29px;
+  text-align: center;
+  color: #FFFFFF;
+  background: #025323;
+  border-radius: 15px;
+  width: 244px;
+  height: 59px;
+  position: absolute;
+  top: 419px;
+  left: 42px;
+  margin-top: 16px;`
   return (
-    <div className="hero-container">
+    <div className="hero-wrapper">
       <img className="hero-img" src={heroImage} alt="Person doing yoga" />
       <Heading>Breath in breath out</Heading>
-      <p>Yogistudio offers an overall concept in self-healing with a focus on calm and therapeutic forms of yoga.</p>
-      <label>
-        <btn type="button" id="bookAClass">Book a class</btn>
-      </label>
+      <Text>Yogistudio offers an overall concept in self-healing with a focus on calm and therapeutic forms of yoga.</Text>
+      <Button type="button" id="bookAClass">Book a class</Button>
     </div>
   )
 }
