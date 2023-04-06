be93a3e11e4a8aa41ed837eef3f621e24f7647e9
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-04-06 17:07:11 +0200

    added content to nav

--- src/compontes/Navbar.js
+++ src/compontes/Navbar.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="top-nav">
+      <h1>FITEASY</h1>
+      <ul>
+        <li>
+          <a href="#">Benefits</a>
+        </li>
+        <li>
+          <a href="#">Workouts</a>
+        </li>
+        <li>
+          <a href="#">Activity</a>
+        </li>
+        <li>
+          <a href="#">Community</a>
+        </li>
+      </ul>
+      <button type="button">Download app</button>
+      <button type="button">🍔</button>
+    </nav>
+  );
 };
 
 export default Navbar;
