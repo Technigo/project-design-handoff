0e55b4fb4281e47d1604417640fcc9f9b88425bd
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-06 09:41:30 +0200

    Update h1 and p in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { TopRightButton, BottomCenterButton } from './Button'
+import { TopRightButton, BottomCenterButton } from './Global'
 import heroImage from '../assets/hero.jpg';
 
 const HeroContainer = styled.div`
@@ -10,32 +10,45 @@ const HeroContainer = styled.div`
   color: #03092E;
   display: flex;
   justify-content: center;
-  align-items: center;
-  height: 900px;
+  align-items: flex-end; /* Align items to the bottom */
+  height: 90vh;
   position: relative; /* Add position relative to the container */
+  padding-bottom: 4rem; /* Add padding to adjust the spacing */
+`;
+
+const TitleContainer = styled.div`
+display:flex;
+flex-direction: row;
+justify-content: space-around;
+align-items: center; /* Align items vertically */
+width: 30%;
+padding: 0 2 rem; /* Add padding to adjust the spacing */
+margin-top: -3rem;
 `;
 
 const Title = styled.h1`
 font-size:2.5rem;
 font-weight: bold;
-margin-top: 32rem;
-margin-left: 0.5;
-width: 17%;
+flex: 0.4;
+word-break: break-word;
+margin-top: 0; /* Reset top margin */
 `;
 
 const Description = styled.p`
-font-size: 1.5rem;
+font-size: 1rem;
 line-height: 1.5;
-margin-bottom:2rem;
+flex: 0.6;
+word-break: break-word;
+margin-top: 0; /* Reset top margin */
 `;
 
 const Hero = ({ title, description }) => {
   return (
     <HeroContainer image={heroImage}>
-      <div>
+      <TitleContainer>
         <Title>{title}</Title>
         <Description>{description}</Description>
-      </div>
+      </TitleContainer>
       <TopRightButton>Try for Free</TopRightButton>
       <BottomCenterButton>Try for Free</BottomCenterButton>
     </HeroContainer>
