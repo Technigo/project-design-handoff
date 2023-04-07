5f468dc1e0380648afe8cdfc9c8256a3f4a31ea7
GG_Cugs <giorgio.cugusi@gmail.com>
2023-04-07 15:36:25 +0200

    carousel created little bag in mobile to fix

--- src/components/CTAsection.js
+++ src/components/CTAsection.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Button } from './Button';
 import './CTAsection.css';
 
 export const CTAsection = () => {
-  const navigate = useNavigate(); // Initialize the navigate function from react-router-dom
-
-  const handleButtonClick = () => {
-    navigate('/pricingplan'); // Navigate to the Pricingplan component
-  };
   return (
     <div className="cta-background">
-      <Button onClick={handleButtonClick}>
+      <Button>
         Start your 7-day free trial
       </Button>
     </div>
--- src/components/Nutrition.js
+++ src/components/Nutrition.js
@@ -1,17 +1,36 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect } from 'react';
 import './Nutrition.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import cake from '../img/cake.png';
 import poke from '../img/poke.png';
 import pizza from '../img/pizza.png';
 
 export const Nutrition = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    centerMode: true,
+    arrows: true,
+    className: 'nutrition-slick-carousel'
+  };
+
+  useEffect(() => {
+    import('slick-carousel').then(() => {});
+  }, []);
+
   return (
     <div className="nutrition-background">
       <div className="nutrition-text">
         <h1 className="nutrition-bigtext">Your Personalized Nutrition Plan</h1>
         <p className="nutrition-paragraph">Receive a personalized nutrition plan that aligns with your dietary preferences, health concerns or fitness objectives. The plan promotes a well-balanced and nourishing diet, and includes access to healthy recipes, inspiration and meal plans to support you on your wellness journey.</p>
       </div>
-      <div className="recipe-carousel">
+      <Slider {...settings}>
         <div className="recipe-card">
           <div className="nutrition-carousel">
             <div className="nutrition-carousel-text">
@@ -19,7 +38,7 @@ export const Nutrition = () => {
               <p className="nutrition-carousel-time">15 min</p>
             </div>
             <div className="nutrition-image">
-              <img src={poke} alt="cake" />
+              <img src={poke} alt="poke" className="nutrition-carousel-image" />
             </div>
           </div>
         </div>
@@ -30,7 +49,7 @@ export const Nutrition = () => {
               <p className="nutrition-carousel-time">55 min</p>
             </div>
             <div className="nutrition-image">
-              <img src={cake} alt="cake" />
+              <img src={cake} alt="cake" className="nutrition-carousel-image" />
             </div>
           </div>
         </div>
@@ -41,11 +60,11 @@ export const Nutrition = () => {
               <p className="nutrition-carousel-time">55 min</p>
             </div>
             <div className="nutrition-image">
-              <img src={pizza} alt="cake" />
+              <img src={pizza} alt="pizza" className="nutrition-carousel-image" />
             </div>
           </div>
         </div>
-      </div>
+      </Slider>
     </div>
   )
 }
