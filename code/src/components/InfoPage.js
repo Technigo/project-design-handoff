3decfc268904037da365a73373a02bf4fc4a1209
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-07 10:54:04 +0200

    info section

--- code/src/components/InfoPage.js
+++ code/src/components/InfoPage.js
@@ -1,10 +1,11 @@
 /* eslint-disable max-len */
 import React from 'react';
 import CircleCheck from './NamedIcons/CircleCheck';
+import './InfoPage.css';
 
 const InfoPage = () => {
   return (
-    <>
+    <div className="info-page-wrapper">
       <h1>Personal Coach support and flexibility of Online Training</h1>
       <h3>Your personal Coach</h3>
 
@@ -17,7 +18,7 @@ const InfoPage = () => {
       <p><CircleCheck />Fit the sessions into your schedule</p>
       <p><CircleCheck />Never boring</p>
 
-      <img src="../assets/stretchguy.jpg" alt="Man stretching" style={{ maxWidth: '50vw' }} />
+      <img src="../assets/stretchguy.jpg" alt="Man stretching" style={{ maxWidth: '80vw' }} />
 
       <h1>Your preferred workout and diet plan</h1>
 
@@ -28,11 +29,11 @@ const InfoPage = () => {
 
       <p><CircleCheck />We offer different healthy diets</p>
       <p><CircleCheck />Diet plans are customised according to your health conditions and background</p>
-
-      <img src="../assets/workout.jpg" alt="workout equipment" style={{ maxWidth: '50vw' }} />
-      <img src="../assets/dietplan.png" alt="Healthy dinner" style={{ maxWidth: '50vw' }} />
-
-    </>
+      <div className="image-wrapper">
+        <img src="../assets/workout.jpg" alt="workout equipment" style={{ maxWidth: '80vw' }} />
+        <img src="../assets/dietplan.png" alt="Healthy dinner" style={{ maxWidth: '50vw', position: 'absolute', top: '50%', left: '45%' }} />
+      </div>
+    </div>
   )
 }
 
