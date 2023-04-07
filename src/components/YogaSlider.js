5fd084096e317112b5b41bb61104d10a68c78b88
Vio Szabo <vio@vioszabo.se>
2023-04-07 20:59:19 +0200

    toggle button on videos

--- src/components/YogaSlider.js
+++ src/components/YogaSlider.js
@@ -1,22 +1,23 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import React from "react";
+import React, { useRef } from "react";
 import { FreeMode } from "swiper";
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css';
 import "swiper/css/free-mode";
 import styled from 'styled-components'
+import videoSrc from '../videos/video.mp4'
+import BtnSrc from '../icons/playbtn.svg'
 
-export const YogaSlider = () => {
-  const SliderContainer = styled.div`
+const SliderContainer = styled.div`
   display: flex;
   `
 
-  const StyledP = styled.p`
+const StyledP = styled.p`
   color: #02393F;
   font-size: 13px;
   overflow: hidden;
   `
-  const StyledDescriptionWrapper = styled.div`
+const StyledDescriptionWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   margin-right: 8px;
@@ -24,18 +25,61 @@ export const YogaSlider = () => {
   width: 220px;
   `
 
-  const StyledVideo = styled.video`
+const StyledVideo = styled.video`
   object-fit: cover;
   width: 226px;
   height: 189px;
   object-position: center;
   border-radius: 12px;
+  }
   `
 
-  const StyledVideoContainer = styled.div`
+const StyledVideoContainer = styled.div`
   position: relative;
   display: block;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.5);
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    z-index: 2;
+
+    &:hover:before {
+      opacity: 1;
+      z-index: 2;
+    }
+  }
   `
+const StyledBtn = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 20%;
+  z-index: 3;
+  margin: auto;
+  `
+
+export const YogaSlider = () => {
+
+  const vidRefs = useRef([]);
+
+  const handleToggleVideo = (index) => {
+    const vidRef = vidRefs.current[index];
+    if (vidRef.paused) {
+      vidRef.play();
+    } else {
+      vidRef.pause();
+    }
+  }
+
+  const setVidRef = (ref, index) => {
+    vidRefs.current[index] = ref;
+  }
 
   return (
     <SliderContainer>
@@ -45,7 +89,6 @@ export const YogaSlider = () => {
         slidesPerView={1.5}
         freeMode
         loop
-
         modules={[FreeMode]}
         className="YogaSwiper">
         <SwiperSlide>
@@ -54,7 +97,10 @@ export const YogaSlider = () => {
               <StyledP>Yinyoga to calm down</StyledP>
               <StyledP>15 min</StyledP>
             </StyledDescriptionWrapper>
-            <StyledVideo controls src="video/video.mp4" poster="img/videoposter.png"/>
+            <div>
+              <StyledBtn src={BtnSrc} onClick={() => handleToggleVideo(0)} alt="play" />
+              <StyledVideo ref={(ref) => setVidRef(ref, 0)} src={videoSrc} poster="img/videoposter.png"/>
+            </div>
           </StyledVideoContainer>
         </SwiperSlide>
         <SwiperSlide>
@@ -63,7 +109,10 @@ export const YogaSlider = () => {
               <StyledP>Yinyoga to calm down</StyledP>
               <StyledP>15 min</StyledP>
             </StyledDescriptionWrapper>
-            <StyledVideo src="video/video.mp4" poster="img/videoposter.png"/>
+            <div>
+              <StyledBtn src={BtnSrc} onClick={() => handleToggleVideo(1)} alt="play" />
+              <StyledVideo ref={(ref) => setVidRef(ref, 1)} src={videoSrc} poster="img/videoposter.png"/>
+            </div>
           </StyledVideoContainer>
         </SwiperSlide>
         <SwiperSlide>
@@ -72,7 +121,8 @@ export const YogaSlider = () => {
               <StyledP>Yinyoga to calm down</StyledP>
               <StyledP>15 min</StyledP>
             </StyledDescriptionWrapper>
-            <StyledVideo src="video/video.mp4" poster="img/videoposter.png"/>
+            <StyledBtn src={BtnSrc} onClick={() => handleToggleVideo(2)} alt="play" />
+            <StyledVideo ref={(ref) => setVidRef(ref, 2)} src={videoSrc} poster="img/videoposter.png"/>
           </StyledVideoContainer>
         </SwiperSlide>
         <SwiperSlide>
@@ -81,11 +131,11 @@ export const YogaSlider = () => {
               <StyledP>Yinyoga to calm down</StyledP>
               <StyledP>15 min</StyledP>
             </StyledDescriptionWrapper>
-            <StyledVideo src="video/video.mp4" poster="img/videoposter.png"/>
+            <StyledBtn src={BtnSrc} onClick={() => handleToggleVideo(2)} alt="play" />
+            <StyledVideo ref={(ref) => setVidRef(ref, 2)} src={videoSrc} poster="img/videoposter.png"/>
           </StyledVideoContainer>
         </SwiperSlide>
-        
       </Swiper>
     </SliderContainer>
   );
-};
+}
