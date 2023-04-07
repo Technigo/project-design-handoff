bc135bd8dcbc99f974b3ef7ff8598d2ee7b7ebca
Johanna Leonsson <johanna@leonsson.se>
2023-04-07 15:52:37 +0200

    updated header structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,18 +4,25 @@ import { SignUpBtn } from '../Buttons'
 // import { Navbar } from './Navbar'
 import './Header.css';
 
-const Logo = styled.img`
-    width: 35px;
-    height: 48px;
+const Logo = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 15px;
+`
+const Navigation = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
 `
 
 export const Header = () => {
   return (
-    <section className="navbar-cont">
-      <div className="logo-cont">
-        <Logo src="./logo/Logo.svg" />
-        <i className="fa-sharp fa-regular fa-magnifying-glass" />
-      </div>
+    <Navigation>
+      <Logo>
+        <img src="./logo/Logo.svg" alt="Logo" />
+        <i className="fa-sharp fa-solid fa-magnifying-glass" />
+      </Logo>
       <section className="intro-text">
         <p>Santulan Power Yoga Stockholm</p>
         <h1>Move your body <br />
@@ -24,6 +31,6 @@ export const Header = () => {
         <SignUpBtn />
         <p>Want to know more about us?</p>
       </section>
-    </section>
+    </Navigation>
   )
 }
