ea231a2ad75cfba028641890fdc7e9977b96b14b
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-07 11:40:53 +0200

    adding coaches

--- code/src/App.js
+++ code/src/App.js
@@ -4,6 +4,7 @@ import ClientReviewsCarousel from './components/ClientReviews'
 import PageTwo from './components/PageTwo'
 import HeroPage from './components/HeroPage'
 import NavBar from './components/NavBar/NavBar'
+import CoachesCarousel from './components/Coaches'
 // import styled from 'styled-components'
 
 export const App = () => {
@@ -14,6 +15,7 @@ export const App = () => {
       <PageTwo />
       <InfoPage />
       <ClientReviewsCarousel />
+      <CoachesCarousel />
     </div>
   )
 }
---
+++ code/src/components/Coaches.js
@@ -0,0 +1,50 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+import './Coaches.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const CoachesCarousel = () => {
+  const responsiveCoaches = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+  return (
+    <div className="coaches-container">
+      <h1>Get to know our coaches</h1>
+      <Carousel responsive={responsiveCoaches}>
+        <div className="coaches-card">
+          <img src="../assets/Maggi.jpg" alt="picture of Maggi" />
+        </div>
+        <div className="coaches-card">
+          <img src="../assets/Helia.jpg" alt="picture of Helia" />
+        </div>
+        <div className="coaches-card">
+          <img src="../assets/Viggo.jpg" alt="picture of Viggo" />
+        </div>
+        <div className="coaches-card">
+          <img src="../assets/Lili.jpg" alt="picture of Lili" />
+        </div>
+        <div className="coaches-card">
+          <img src="../assets/Liz.jpg" alt="picture of Liz" />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CoachesCarousel;
