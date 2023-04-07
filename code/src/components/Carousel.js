f0dc2c96ae0640f6977d3577ef2fa175df8655e3
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-04-08 00:08:04 +0200

    css

--- code/src/components/Carousel.js
+++ code/src/components/Carousel.js
@@ -8,31 +8,36 @@ const cardboxes = [
   {
     id: 1,
     text: 'Fill the simple form',
+    text2: 'Fill the primary questionnaire and we will contact you to know more about your health condition, fitness background and your special needs. ',
     logo: '/images/list.png'
   },
   {
     id: 2,
     text: 'Get matched with a certified coach',
+    text2: 'You then try our free 1-1 service to meet a coach who matched with your preferences and requirements ',
     logo: '/images/users.png'
   },
   {
     id: 3,
     text: 'Enjoy your journey',
+    text2: 'Your personal coach design a training plan and diet tailored to your individual needs, push, motivate and support you to make your life happier and healthier ',
     logo: '/images/yoga-pose.png'
   }
 ];
 
-const CarouselSection = ({ text, logo }) => (
+const CarouselSection = ({ text, text2, logo }) => (
   <div className="carousel-wrapper" style={{ backgroundColor: '#D7CBFF' }}>
     <div className="carousel-box">
       <img src={logo} alt="Logo" style={{ width: '50px' }} />
-      <p>{text}</p>
+      <p style={{ fontWeight: 'bold' }}>{text}</p>
+      <p>{text2}</p>
     </div>
   </div>
 );
 
 const Section = () => {
-  const renderSlide = ({ text, logo }) => <CarouselSection text={text} logo={logo} />;
+  // eslint-disable-next-line max-len
+  const renderSlide = ({ text, text2, logo }) => <CarouselSection text={text} text2={text2} logo={logo} />;
 
   return (
     <div className="section">
--- code/src/components/Hero.js
+++ code/src/components/Hero.js
@@ -5,41 +5,63 @@ export const HeroSection = () => {
   return (
     <Container>
       <HeaderText>Online Healthy Diet Strong Body</HeaderText>
-      <SubText>Level up your life to be happier and healthier</SubText>
+      <SubText>Level up your life to the happier and healthier
+        with help of the certified personal trainers and
+        nutrition coaches based on your personal circumstance
+      </SubText>
       <Button>Try for Free</Button>
     </Container>
   );
 };
 const Container = styled.section`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  widht: 100vw;
-  height: 150vh;
+  width: 100%;
+  height: 80vh;
   background-image: url('/images/hero-image-smaller.png');
   background-size: cover;
   background-position: center;
 
   @media only screen and (max-width: 600px) {
     background-image: url('/images/mobile-hero-image.png');
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
   }
 `;
 
 const HeaderText = styled.h1`
-  font-size: 4rem;
+  font-size: 2rem;
   font-weight: bold;
   color: #fff;
+
+  @media only screen and (max-width: 600px) {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    margin-right: 40px;
+  }
 `;
 
 const SubText = styled.p`
-  font-size: 2rem;
+  font-size: 1.5rem;
   color: #fff;
+  padding: 20px;
+
+  @media only screen and (max-width: 600px) {
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    padding-bottom: 90px;
+  }
 `;
 
 const Button = styled.button`
-  font-size: 1.5rem;
-  padding: 1rem 2rem;
+  font-size: 1.2rem;
+  padding: 0.8rem 1.5rem;
   border-color: orange;
   border-radius: 10px ;
   background-color: transparent;
@@ -50,5 +72,8 @@ const Button = styled.button`
   &:hover {
     background-color: #fff;
     color: #f44336;
+  }
+  @media only screen and (max-width: 600px) {
+    margin-bottom: 20px;
   }
 `;
