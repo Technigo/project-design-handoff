fa130f367f39e1df0bd81b3c4f8485ec5a138ada
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-07 13:18:13 +0200

    renaming imports

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import FAQuestions from '@components/Faq/FAQText'
 import InfoPage from './components/InfoPage'
 import ClientReviewsCarousel from './components/ClientReviews'
 import PageTwo from './components/PageTwo'
 import HeroPage from './components/HeroPage'
 import NavBar from './components/NavBar/NavBar'
 import CoachesCarousel from './components/Coaches'
-import FAQuestions from './components/Faq/FaqText'
 // import styled from 'styled-components'
 
 export const App = () => {
