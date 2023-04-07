3303313688bdc5f771cd1f3b8e936e5a7593fe50
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-07 21:01:24 +0200

    signin page

--- code/src/components/Signup.js
+++ code/src/components/Signup.js
@@ -16,18 +16,33 @@ const SignupPageForm = () => {
       className="signup-outer-wrapper"
       style={{ backgroundImage: "url('./assets/forminput.jpg')",
         backgroundSize: 'cover',
-        backgroundPosition: 'center' }}>
-      <h1><span className="highlighted-text">Book </span>&nbsp;a free trial.</h1>
+        backgroundPosition: 'center',
+        height: '80vh',
+        position: 'absolute' }}>
+
+      <h1><span className="highlighted-text">Book </span>&nbsp;a free trial</h1>
       <h3>Tell us about your preferences and we offer you a tailored plan for healthier life </h3>
       <div className="signup-inner-wrapper">
-        <form>
+        <form className="form">
           <input type={name} onChange={(e) => setName(e.target.value)} placeholder="Your Name" id="name" className="name" />
           <input type={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" id="email" className="email" />
           <input type={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Phone" id="phone" className="phone" />
-          <input type={location} onChange={(e) => setLocation(e.target.value)} placeholder="Your current location (city,town)" id="email" className="email" />
+          <input type={location} onChange={(e) => setLocation(e.target.value)} placeholder="Your current location (city,town)" id="location" className="location" />
+
+          <div className="checkbox-container">
+            <label htmlFor="accept">
+              <input className="custom-checkbox" type="checkbox" name="accept" id="accept" />
+
+              <p> I am over 13 and accept the<span className="highlighted-text2">&nbsp;privacy policy, terms of service,</span>
+              &nbsp;and<span className="highlighted-text2">&nbsp;code of conduct</span>
+              </p>
+            </label>
+          </div>
+
         </form>
-        <button className="tryforfree" type="submit" onClick={handleButtonClick}>Get started free trial</button>
+        <button className="free-trial-button" type="submit" onClick={handleButtonClick}>Get started free trial</button>
       </div>
+
     </div>
   )
 };
