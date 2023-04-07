24e5fe49aa7e6695046ce0d24d9274046e5e1b7c
Emma Engvall <emma.engvall@ica.se>
2023-04-07 22:31:08 +0200

    added styling for components

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,28 +3,47 @@ import './Form.css';
 
 const Form = () => {
   return (
-    <div className="register">
-      <h2>Register at Raise Studio </h2>
-    </div>
+    <section>
+      <div className="register">
+        <h2>Register at Raise Studio </h2>
+        <h3>Create an Account to Book Barre Classes and Buy Credits.</h3>
+        <p className="login">Already have an account? Login</p>
+      </div>
+      <div className="form">
+        <form className="labels">
+          <label className="name"> First Name
+            <input type="text"/>
+          </label>
+          <label> Last Name
+            <input type="text"/>
+          </label>
+          <label> Phone number
+            <input type="number"/>
+          </label>
+          <label> Email (username)
+            <input type="email"/>
+          </label>
+          <label> Password
+            <input type="password"/>
+          </label>
+          <label>
+            <input type="checkbox"/>
+            I want to receive Raise Studio’s Newsletter!
+          </label>
+          <label>
+            <input type="checkbox"/>
+            I have read and understood the Terms & Conditions and Privacy Policy.
+          </label>
+          <button className="joinBtn2" type="button">Join Raise Studio</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
 export default Form
 
-/*<Div className=Register> 
-<h4>Create an Account to Book Barre Classes and Buy Credits.</h4>
-<p>Already have an account? <span id="underline">Login</span> Calling the span text-decoration:underline in CSS.
-</>
-
-<Div className=Form>
-Form
-First Name (Description)
-Last Name
-Phone number
-Email (username)
-Password
-<type checkbox> I want to receive Raise Studio’s Newsletter! </>
-<type checkbox> I have read and understood the Terms & Conditions and Privacy Policy. </>
-<button> Join Raise Studio </button> 
-</>
+/*
+Link for Login
+https://www.w3schools.com/css/css_link.asp
 */
