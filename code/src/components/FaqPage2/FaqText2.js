cc05ade08b2511780a030302bbc4558b652cb894
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-07 15:45:51 +0200

    faq page

--- code/src/components/FaqPage2/FaqText2.js
+++ code/src/components/FaqPage2/FaqText2.js
@@ -1,4 +1,4 @@
-/* eslint-disable array-callback-return */
+
 import React, { useState } from 'react';
 import './FaqText2.css';
 import FAQ from './FaqFunction';
@@ -6,36 +6,43 @@ import FAQ from './FaqFunction';
 const FAQuestions = () => {
   const [faqs, setfaqs] = useState([
     {
+      id: 1,
       question: 'What device do I need to join the program?',
       answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli',
       open: true
     },
     {
+      id: 2,
       question: 'What happens if I am not satisfied with your service?',
       answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli',
       open: true
     },
     {
+      id: 3,
       question: 'Can I change my coach in the middle of the program?',
       answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli',
       open: true
     },
     {
+      id: 4,
       question: 'What information do you need to tailor a diet plan for me?',
       answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli',
       open: true
     },
     {
+      id: 5,
       question: 'What happens if I miss a session?',
       answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli',
       open: true
     },
     {
+      id: 6,
       question: 'Can I record my online sessions?',
       answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli',
       open: true
     },
     {
+      id: 7,
       question: 'Do I have acess to my coach outside of the session time?',
       answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli',
       open: true
@@ -43,19 +50,18 @@ const FAQuestions = () => {
 
   ]);
 
-  const toggleFAQ = (index) => {
-    setfaqs(faqs.map((faq, i) => {
-      if (i === index) { faq.open = !faq.open } else {
-        faq.open = false;
-      }
+  const toggleFAQ = (id) => {
+    setfaqs(faqs.map((faq) => {
+      if (faq.id === id) return { ...faq, open: !faq.open } 
+      return faq
     }))
   }
   return (
     <div className="faq-wrapper">
       <h1>FAQs</h1>
       <div className="faqs">
-        {faqs.map((faq, i) => (
-          <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ} />
+        {faqs.map((faq) => (
+          <FAQ key={faq.id} faq={faq} toggleFAQ={toggleFAQ} />
         ))}
       </div>
     </div>
