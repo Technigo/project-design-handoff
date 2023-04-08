60d893870cf401985060eb1f7c23c5d4aad5c8e6
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-08 16:05:51 +0200

    ipad responsiveness carousels

--- code/src/components/InfoPage.js
+++ code/src/components/InfoPage.js
@@ -6,32 +6,37 @@ import './InfoPage.css';
 const InfoPage = () => {
   return (
     <div className="info-page-wrapper">
-      <h1>Personal Coach support and flexibility of Online Training</h1>
-      <h3>Your personal Coach</h3>
+      <div className="coach-support">
 
-      <p><CircleCheck />Monitor your progression and technique</p>
-      <p><CircleCheck />Keeps you accountable and motivated</p>
+        <h1>Personal Coach support and flexibility of Online Training</h1>
+        <h3>Your personal Coach</h3>
 
-      <h3>Online Workout</h3>
+        <p><CircleCheck />Monitor your progression and technique</p>
+        <p><CircleCheck />Keeps you accountable and motivated</p>
 
-      <p><CircleCheck />Save time and resources</p>
-      <p><CircleCheck />Fit the sessions into your schedule</p>
-      <p><CircleCheck />Never boring</p>
+        <h3>Online Workout</h3>
 
-      <img src="../assets/stretchguy.jpg" alt="Man stretching" style={{ maxWidth: '80vw' }} />
+        <p><CircleCheck />Save time and resources</p>
+        <p><CircleCheck />Fit the sessions into your schedule</p>
+        <p><CircleCheck />Never boring</p>
 
-      <h1>Your preferred workout and diet plan</h1>
+        <img src="../assets/stretchguy.jpg" alt="Man stretching" id="man-stretching" />
+      </div>
 
-      <p><CircleCheck />We offer various types of workouts</p>
-      <p><CircleCheck />Trainings are conducted by certified coaches and tailored according to your personal needs </p>
+      <div className="workout-plan">
+        <h1>Your preferred workout and diet plan</h1>
 
-      <h3>Diet plan</h3>
+        <p><CircleCheck />We offer various types of workouts</p>
+        <p><CircleCheck />Trainings are conducted by certified coaches and tailored according to your personal needs </p>
 
-      <p><CircleCheck />We offer different healthy diets</p>
-      <p><CircleCheck />Diet plans are customised according to your health conditions and background</p>
-      <div className="image-wrapper">
-        <img src="../assets/workout.jpg" alt="workout equipment" style={{ maxWidth: '80vw' }} />
-        <img src="../assets/dietplan.png" alt="Healthy dinner" style={{ maxWidth: '50vw', position: 'absolute', top: '50%', left: '45%' }} />
+        <h3>Diet plan</h3>
+
+        <p><CircleCheck />We offer different healthy diets</p>
+        <p><CircleCheck />Diet plans are customised according to your health conditions and background</p>
+        <div className="image-wrapper">
+          <img src="../assets/workout.jpg" alt="workout equipment" style={{ maxWidth: '80vw' }} />
+          <img src="../assets/dietplan.png" alt="Healthy dinner" style={{ maxWidth: '50vw', position: 'absolute', top: '50%', left: '45%' }} />
+        </div>
       </div>
     </div>
   )
