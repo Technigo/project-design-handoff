5c918c32e7414717d2bab90c977a630ced9d47f8
Vera Sjunnesson <Vera@admins-MacBook-Pro.local>
2023-04-08 16:17:59 +0200

    testing swiper

--- src/components/parts/TextCarousel.js
+++ src/components/parts/TextCarousel.js
@@ -1,60 +1,20 @@
 import React from 'react'
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
-
-import { Headline2, BodyText } from 'components/styles/Text'
-import { TextContainer } from 'components/styles/Section'
-// register Swiper custom elements
-register();
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
 export const TextCaousel = () => {
   return (
-    <swiper-container slides-per-view="3" speed="500" loop="true" css-mode="true" height="171px" overflow="hidden">
-      <swiper-slide style={{ width: '287px', height: '171px' }}>
-        <TextContainer flexcolumn>
-          <Headline2>
-            Milestones
-          </Headline2>
-          <BodyText>
-            Don&apos;t have any personal goals yet?
-            Challenge yourself by completing our pre-made
-            milestones that definitley will keep you motivated
-          </BodyText>
-        </TextContainer>
-      </swiper-slide>
-      <swiper-slide style={{ width: '287px', height: '171px' }}>
-        <TextContainer flexcolumn>
-          <Headline2>
-            Stats
-          </Headline2>
-          <BodyText>
-            View your general stats and keep track of your achievments
-          </BodyText>
-        </TextContainer>
-      </swiper-slide>
-      <swiper-slide style={{ width: '287px', height: '171px' }}>
-        <TextContainer flexcolumn>
-          <Headline2>
-            Schedule
-          </Headline2>
-          <BodyText>
-            Schedule and monitor all your workouts in the calender
-            and set up a personal reminder to keep you going
-          </BodyText>
-        </TextContainer>
-      </swiper-slide>
-      <swiper-slide style={{ width: '287px', height: '171px' }}>
-        <TextContainer flexcolumn>
-          <Headline2>
-            Personal
-          </Headline2>
-          <BodyText>
-            Track any personal fitness activities you
-            accomplish by adding them you your calender
-          </BodyText>
-        </TextContainer>
-      </swiper-slide>
-      ....
-    </swiper-container>
-  )
+    <Swiper
+      className="swiper-container"
+      id="swiper"
+      slidesPerView={1.5}
+      // slidesPerColumn={2}
+      // slidesPerColumnFill="row"
+      spaceBetween={30}>
+      <SwiperSlide className="slide">Slide 1</SwiperSlide>
+      <SwiperSlide className="slide">Slide 2</SwiperSlide>
+      <SwiperSlide className="slide">Slide 3</SwiperSlide>
+      <SwiperSlide className="slide">Slide 4</SwiperSlide>
+    </Swiper>
+  );
 }
