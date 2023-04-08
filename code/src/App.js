177eea5a61792abee01cde75d10a78f011ae5818
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-04-08 19:55:28 +0200

    added footer

--- code/src/App.js
+++ code/src/App.js
@@ -3,6 +3,7 @@ import { HeroSection } from 'components/Hero'
 import { Navbar } from 'components/Navbar'
 import Section from 'components/Carousel'
 import Contact from 'components/Contact'
+import Footer from 'components/Footer'
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <HeroSection />
       <Section />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ code/src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../footer.css';
+import logo from '../images/logo.png';
+import facebookIcon from '../images/facebook.png';
+import twitterIcon from '../images/twitter-sign.png';
+import instagramIcon from '../images/instagram.png';
+import youtubeIcon from '../images/youtube.png';
+import pinterestIcon from '../images/pinterest.png';
+
+const Footer = () => {
+  return (
+    <footer className="footer" style={{ backgroundColor: '#03092E' }}>
+      <div className="footer-content">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+          <p className="logo-text">FOR A HEALTHIER AND FITTER PLANET</p>
+        </div>
+        <div className="links-container">
+          <ul>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Coaches</a></li>
+            <li><a href="#">Club</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Shop</a></li>
+          </ul>
+        </div>
+        <div className="social-icons-container">
+          <a href="#"><img src={facebookIcon} alt="Facebook" /></a>
+          <a href="#"><img src={twitterIcon} alt="Twitter" /></a>
+          <a href="#"><img src={instagramIcon} alt="Instagram" /></a>
+          <a href="#"><img src={youtubeIcon} alt="YouTube" /></a>
+          <a href="#"><img src={pinterestIcon} alt="Pinterest" /></a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
