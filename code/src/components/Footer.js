8ac06422bc9d2a6e6cbbe0cf3165d393da6ece1f
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-08 13:26:04 +0200

    finishing footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -16,57 +16,43 @@ const FooterPage = () => {
         <h3>FOR A HEALTHEIR AND FITTER PLANET</h3>
       </div>
 
-      <div className="About-us-wrapper">
+      <div className="About-us">
         <h2>About Us</h2>
-        <ul>
-          <li>
-            <a className="footer-menu-item" href="#services">Services</a>
-          </li>
-          <li>
-            <a className="footer-menu-item" href="#coaches">Coaches</a>
-          </li>
-          <li>
-            <a className="footer-menu-item" href="#club">Club</a>
-          </li>
-          <li>
-            <a className="footer-menu-item" href="#FAQs">FAQs</a>
-          </li>
-          <li>
-            <a className="footer-menu-item" href="#shop">Shop</a>
-          </li>
-        </ul>
+
+        <a className="footer-menu-item" href="#services">Services</a>
+
+        <a className="footer-menu-item" href="#coaches">Coaches</a>
+
+        <a className="footer-menu-item" href="#club">Club</a>
+
+        <a className="footer-menu-item" href="#FAQs">FAQs</a>
+
+        <a className="footer-menu-item" href="#shop">Shop</a>
+
       </div>
 
-      <div className="contact-us-footer">
-        <h2>Email: hello@selfhealth.com</h2>
-        <h2>Phone: +46(08)562 888 00 </h2>
-      </div>
+      <div className="footer-page-inner-wrapper">
+        <div className="contact-us-footer">
+          <h2>Contact Us</h2>
+          <h2>Email: hello@selfhealth.com</h2>
+          <h2>Phone: +46(08)562 888 00 </h2>
+        </div>
 
-      <div className="icons-wrapper">
-        <FacebookIcon className="icon" />
-        <TwitterIcon className="icon" />
-        <InstagramIcon className="icon" />
-        <YoutubeIcon className="icon" />
-        <PinterestIcon className="icon" />
-      </div>
+        <div className="icons-wrapper">
+          <FacebookIcon style={{ display: 'flex', height: '2rem', fill: 'white', marginRight: '10px', marginTop: '5px' }} />
+          <TwitterIcon style={{ height: '2rem', color: 'white', marginRight: '10px', marginTop: '5px' }} />
+          <InstagramIcon style={{ height: '2rem', color: 'white', marginRight: '10px', marginTop: '5px' }} />
+          <YoutubeIcon style={{ height: '2rem', color: 'white', marginRight: '10px', marginTop: '5px' }} />
+          <PinterestIcon style={{ height: '2rem', color: 'white', marginRight: '10px', marginTop: '5px' }} />
+        </div>
 
-      <div className="policy-links-container">
-        <ul>
-          <li>
-            <a className="policy-links" href="#privacypolicy">Privacy Policy</a>
-          </li>
-          <li>
-            <a className="policy-links" href="#terms">Terms of Service</a>
-          </li>
-          <li>
-            <a className="policy-links" href="#code">Code of Conduct</a>
-          </li>
-          <li>
-            <a className="policy-links" href="#refund">Refund Policy</a>
-          </li>
-        </ul>
+        <div className="policy-links-container">
+          <a className="policy-links" href="#privacypolicy">Privacy Policy</a>
+          <a className="policy-links" href="#terms">Terms of Service</a>
+          <a className="policy-links" href="#code">Code of Conduct</a>
+          <a className="policy-links" href="#refund">Refund Policy</a>
+        </div>
       </div>
-
       <div>
         <hr className="white-line" />
         <a className="copyright" href="#copyright">Copyright 2023 SelfHEALth, All rights Reserved</a>
