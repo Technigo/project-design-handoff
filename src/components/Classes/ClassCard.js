666e20b4648fd7e7f7ba3544a943e5f3ce4145fb
Johanna Leonsson <johanna@leonsson.se>
2023-04-09 22:12:56 +0200

    classes slider size adjusted

--- src/components/Classes/ClassCard.js
+++ src/components/Classes/ClassCard.js
@@ -6,17 +6,17 @@ export const Slider = () => {
   return (
     <section className="yoga-container">
       <AwesomeSlider bullets={false}>
-        <div className="yoga-style">
+        <div className="yoga-card">
           <h3>Power Yoga 60</h3>
           <img src="./img/yoga-1.png" alt="Power Yoga 60" />
         </div>
 
-        <div className="yoga-style">
+        <div className="yoga-card">
           <h3>Power Yoga 75</h3>
           <img src="./img/yoga-2.png" alt="Power Yoga 75" />
         </div>
 
-        <div className="yoga-style">
+        <div className="yoga-card">
           <h3>Yoga Strong</h3>
           <img src="./img/yoga-3.png" alt="Yoga Strong" />
         </div>
