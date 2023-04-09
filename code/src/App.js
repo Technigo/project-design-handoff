225962e3874f8e19388359b090dc612e0bce9835
Alexander-Gabor <gaboralexanderalexgmail.com>
2023-04-09 23:41:14 +0200

    Footer added

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from 'components/Footer'
 import Header from './components/Header'
 import Landing from './components/Landing'
 
@@ -7,6 +8,7 @@ export const App = () => {
     <div className="App">
       <Header />
       <Landing />
+      <Footer />
     </div>
   )
 }
