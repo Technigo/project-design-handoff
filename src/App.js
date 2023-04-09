76ae7fcf10ca4ffd9c0db6884c811ea76d093530
Emma Engvall <emma.engvall@ica.se>
2023-04-09 21:43:28 +0200

    corrections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Footer from 'components/Footer';
-import Header from 'components/Header';
-import Hero from 'components/Hero';
-import Join from 'components/Join';
-import Class from 'components/Class';
-import Quiz from 'components/Quiz';
-import Praise from 'components/Praise';
-import Form from 'components/Form';
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+import { Hero } from 'components/Hero';
+import { Join } from 'components/Join';
+import { Class } from 'components/Class';
+import { Quiz } from 'components/Quiz';
+import { Praise } from 'components/Praise';
+import { Form } from 'components/Form';
 
 export const App = () => {
   return (
