5ada3970a6f8aee2eee0b4fa22dad71d0bdba891
Fiona Klacar <fiona.klacar@gmail.com>
2023-04-09 10:06:07 +0200

    made the header and hero sections responsive for tablet and desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,13 @@ const Header = ({ screenSize }) => {
     <div className="headerWrapper">
       <img className="logo" src={logoUrl} alt="Logo for Raise Studio" />
       <div className="navBar">
+        <div className="navBarDesktop">
+          <button type="button" className="desktopNavButton">Classes</button>
+          <button type="button" className="desktopNavButton">Pricing</button>
+          <button type="button" className="desktopNavButton">About Us</button>
+          <button type="button" className="desktopNavButton">Our Studio</button>
+        </div>
+        <button type="button" className="signInButton">Sign In</button>
         <PrimaryButtonSmall className="primaryButtonSmall" buttonText="Sign Up!" />
         <img className="hamburgerMenu" src="../images/HamburgerMenu.svg" alt="Menu icon - click to expand navigation menu" />
       </div>
