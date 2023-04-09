0421b7ddb50cf08f541c5d2fcf3a8fae8ef5cd38
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-04-10 00:12:18 +0200

    removed some bugs

--- src/compontes/Benefits.js
+++ src/compontes/Benefits.js
@@ -4,35 +4,27 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-closing-bracket-location */
 import React from 'react';
-import breath from '../assets/images/breath.jpg';
-import lunge from '../assets/images/lunge.jpg';
-import breakfast from '../assets/images/breakfast.jpg';
-import swanposition from '../assets/images/swanposition.jpg';
 
 const cards = [
   {
     id: 1,
-    bg: { swanposition },
     titleText: 'Apartment friendly workouts',
     bodyText:
       'No matter where you are or the size of you space, we got a class for you.'
   },
   {
     id: 2,
-    bg: { lunge },
     titleText: 'Structured workout programs',
     bodyText:
       'We’ve got different programs for all levels & result to suit everyone.'
   },
   {
     id: 3,
-    bg: { breath },
     titleText: 'Designed to keep you focused',
     bodyText: 'Track your activity & earn badges for completing workouts.'
   },
   {
     id: 4,
-    bg: { breakfast },
     titleText: 'Healthy recipies & more',
     bodyText: 'Search among our recipies & get inspired for a healthy inside.'
   }
