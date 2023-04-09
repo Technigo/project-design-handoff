d2c3751608bbdaac7f21ef1aaaf526a568d6ff8c
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2023-04-10 00:27:13 +0200

    Archive components without the carousel

--- src/components/AchieveTop.js
+++ src/components/AchieveTop.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import './AchieveTopCss.css'
+import Run from '../assets/run.svg'
+import Ball from '../assets/ball.svg'
+import Swim from '../assets/swim.svg'
+import Yoga from '../assets/yoga.svg'
+import Walks from '../assets/walks.svg'
+import Balance from '../assets/balance.svg'
 
 const AchieveTop = () => {
   return (
     <section className="achievetop-container">
-     TOP
+      <div className="text-button-box">
+        <h1 className="header-achieve">
+        FIND ACTIVITIES <br /><span className="header-achieve-size">YOUR STYLE</span>
+        </h1>
+        <button type="button" className="button-achieve primary">Get started
+        </button>
+      </div>
+      <div className="cards-box">
+        <img
+          src={Run}
+          alt="Card run"
+          className="card-image" />
+        <img
+          src={Ball}
+          alt="Card run"
+          className="card-image" />
+        <img
+          src={Swim}
+          alt="Card run"
+          className="card-image" />
+        <img
+          src={Yoga}
+          alt="Card run"
+          className="card-image" />
+        <img
+          src={Walks}
+          alt="Card run"
+          className="card-image" />
+        <img
+          src={Balance}
+          alt="Card run"
+          className="card-image" />
+      </div>
     </section>
   )
 }
