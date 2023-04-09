6ea76a4b14605b486c9058be20238345532d01a8
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-04-09 23:04:56 +0200

    workout component structure and css mobile

--- src/compontes/Workout.js
+++ src/compontes/Workout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import playButton from '../assets/images/playbutton.svg';
 
 const Workout = () => {
   return (
@@ -8,31 +9,38 @@ const Workout = () => {
         <h2>One 5 min workout is better than none.</h2>
       </div>
       <div className="workout-box-container">
-        <div className="box strech">
+        <div className="box strech item-a">
+          <div>
+            <img src={playButton} alt="press button to play" />
+          </div>
           <div>
             <h2>5 min warm up</h2>
             <p>For at home or the gym</p>
-          </div>
-          <div>
             <button type="button">View more</button>
           </div>
         </div>
-        <div className="box">
-          <h1>Healthy made easy</h1>
-          <p>
-            Choose a workout or try out any of our weekly challenges. All our
-            exercises range between 5 to 30 min. Each exercise is accompanied by
-            a detailed written description and video to help you understand and
-            perfect each move.
-          </p>
+        <div className="box item-b healthy">
+          <div>
+            <h1>Healthy made easy</h1>
+            <p>
+              Choose a workout or try out any of our weekly challenges. All our
+              exercises range between 5 to 30 min. Each exercise is accompanied
+              by a detailed written description and video to help you understand
+              and perfect each move.
+            </p>
+          </div>
         </div>
-        <div className="box">
-          <h2>AB workouts</h2>
-          <p>For at home or the gym</p>
+        <div className="box item-c ab">
+          <div>
+            <h2>AB workouts</h2>
+            <p>For at home or the gym</p>
+          </div>
         </div>
-        <div className="box">
-          <h2>Cardio workouts</h2>
-          <p>And so much more...</p>
+        <div className="box item-d cardio">
+          <div>
+            <h2>Cardio workouts</h2>
+            <p>And so much more...</p>
+          </div>
         </div>
       </div>
     </section>
