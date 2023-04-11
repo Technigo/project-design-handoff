edee95a407cbbbe2edf514558d929b03a8f56da5
Ylva Karlsson <121539816+YlvaKarlsson@users.noreply.github.com>
2023-04-11 16:14:20 +0200

    Some more work on the Hero-section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from 'components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Hero } from 'components/Hero/Hero';
 import Footer from './components/Footer';
 
 export const App = () => {
   return (
     <Router>
       <Navbar />
+      <Hero />
       <Routes>
         <Route path="/" />
         <Route path="/products" />
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,27 +1,36 @@
 import React from 'react';
 // import './Hero.css';
 import { StartButton } from 'components/StartButton';
+import {
+  HeroContainer,
+  HeroMainText,
+  ButtonsContainer
+} from './Hero.style';
 
 export const Hero = () => {
   return (
-    <div className="main-content-container">
-      <div className="main-text">
-        <h1>YOUR<br />
+    <HeroContainer>
+      <>
+        <HeroMainText>
+          <h1>YOUR<br />
             WORKOUT<br />
             YOUR<br />
             TIME<br />
             TOGETHER
-        </h1>
+          </h1>
+        </HeroMainText>
+
         <p> Find the activity that suits you and buddy.
               No matter the amount of time you have
               to spare, we have you covered.
               Get started at your personal level.
         </p>
-      </div>
-      <div className="buttons-container">
-        <StartButton cta>Get started</StartButton>
-        <StartButton outlined>Default</StartButton>
-      </div>
-    </div>
+        <ButtonsContainer>
+          {/* <div className="buttons-container"> */}
+          <StartButton cta>Get started</StartButton>
+          <StartButton outlined>Default</StartButton>
+        </ButtonsContainer>
+      </>
+    </HeroContainer>
   )
 }
