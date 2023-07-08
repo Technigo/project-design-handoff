f7c54ed2c20745072c44ca9a352c43b4fe8f3300
AntoniaGranit <antonia.granit@gmail.com>
2023-07-08 12:42:44 +0300

    add classes list to desktop

--- src/Sections/Classes.js
+++ src/Sections/Classes.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import 'react-awesome-slider/dist/styles.css';
 import { PurpleTitle } from '../Components/Fonts';
 import { ClassesSlider } from '../Components/ClassesSlider';
+import { DesktopClasses } from '../Components/DesktopClasses';
 import DownArrow from '../Assets/Icons/DownArrow.svg';
 
 const ClassesDiv = styled.div`
@@ -17,7 +18,8 @@ align-items: center;
 export const Classes = () => {
   return (
     <ClassesDiv>
-      <PurpleTitle>Our Classes &nbsp;<img src={DownArrow} alt="down arrow icon" /></PurpleTitle>
+      <PurpleTitle><b>Our Classes</b> &nbsp;<img src={DownArrow} alt="down arrow icon" /></PurpleTitle>
+      <DesktopClasses />
       <ClassesSlider />
     </ClassesDiv>
   )
