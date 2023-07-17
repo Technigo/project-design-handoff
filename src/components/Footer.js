321c97efd2311587029da290c63bb97c0c50001d
Emma Engvall <emma.engvall@ica.se>
2023-07-17 19:30:18 +0200

    updated styling for footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,29 +3,29 @@ import './Footer.css';
 
 export const Footer = () => {
   return (
-    <section className="footer-container">
-      <div className="logo">
-        <img className="logo" src="./images/logo/LogoSmlLgt.svg" alt="logo" />
+    <section className="footerWrapper">
+      <div className="companyInfo">
+        <img className="logoFooter" src="./images/logo/LogoSmlLgt.svg" alt="Logo for Raise Studio" />
         <p>Vallgatan 6</p>
         <p>223 61, Lund</p>
       </div>
-      <div className="company-info">
+      <div className="company">
         <h5>Company</h5>
         <p>About Us</p>
         <p>Contact Us</p>
         <p>Our Studio</p>
         <p>FAQs</p>
       </div>
-      <div className="classes-info">
+      <div className="classes">
         <h5>Classes</h5>
         <p>Pricing</p>
         <p>Schedule</p>
         <p>Instructors</p>
         <p>About Barre</p>
       </div>
-      <div className="socials">
-        <p className="follow">Follow Us</p>
-        <div className="icons">
+      <div className="iconWrapper">
+        <p className="followUs">Follow Us</p>
+        <div className="smIcons">
           <img className="sm-icon" src="./images/icons_social/_Facebook.svg" alt="Facebook logo" />
           <img className="sm-icon" src="./images/icons_social/_Instagram.svg" alt="Instagram logo" />
           <img className="sm-icon" src="./images/icons_social/_Tiktok.svg" alt="TikTok logo" />
