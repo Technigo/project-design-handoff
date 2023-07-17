5a7f61f9c6316b824fb677c08465ff9a084d7589
Emma Engvall <emma.engvall@ica.se>
2023-07-17 21:07:49 +0200

    design changes for Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,38 +5,53 @@ import './Form.css';
 export const Form = () => {
   return (
     <section>
-      <div className="register">
-        <h2>Register at Raise Studio </h2>
-        <h3>Create an Account to Book Barre Classes and Buy Credits.</h3>
-        <p className="login">Already have an account? Login</p>
+      <div className="registerWrapper">
+        <div className="registerTop" >
+        <h2 className="registerText">Register at Raise Studio </h2>
+        <h3 className="accountText">Create an Account to Book Barre Classes and Buy Credits.</h3>
+        <p className="loginText">Already have an account? 
+        <button type="button" className="loginBtn">Login</button></p>
       </div>
-      <div className="form">
-        <form className="labels">
-          <label className="name"> First Name
-            <input type="text" />
+      <div className="registerMain">
+        <form className="registerForm">
+          <label htmlFor="firstName">
+            <span>First Name</span>
+            <input type="text" id="firstName" name="firstName" required /><br />
           </label>
-          <label className="last-name"> Last Name
-            <input type="text" />
+          <label htmlFor="lastName">
+            <span>Last Name</span>
+            <input type="text" id="lastName" name="lastName" required /><br /> 
           </label>
-          <label className="phone-number"> Phone number
-            <input type="number" />
+          <label htmlFor="phoneNumber">
+            <span>Phone number</span>
+            <input type="tel" id="phoneNumber" name="phoneNumber" required /><br />
           </label>
-          <label className="email"> Email (username)
-            <input type="email" />
+          <label htmlFor="email">
+            <span>Email (username)</span>
+            <input type="email" id="email" name="email" required /><br />
           </label>
-          <label className="password"> Password
-            <input type="password" />
+          <label htmlFor="password">
+            <span>Password</span>
+            <input type="password" id="password" name="password" required /><br />
           </label>
-          <label className="newsletter">
-            <input type="checkbox" />
-            I want to receive Raise Studio’s Newsletter!
-          </label>
-          <label className="terms">
-            <input type="checkbox" />
-            I have read and understood the Terms & Conditions and Privacy Policy.
-          </label>
-          <button className="joinBtn2" type="button">Join Raise Studio</button>
-        </form>
+          <div className="checkboxContainer">
+            <label htmlFor="newsletter">
+              <input type="checkbox" id="newsletter" name="newsletter" value="newsletter" />
+             <span>I want to receive Raise Studio&apos;s Newsletter!</span>
+            </label><br />
+            <label htmlFor="terms">
+              <input type="checkbox" id="terms" name="terms" value="terms" />
+             <span>I have read and understood the 
+              <button type="button" className="registerBtn">Terms & Conditions</button> and
+              <button type="button" className="registerBtn">Privacy Policy.</button>
+             </span>
+            </label>
+            </div>
+            <div className="joinBtnWrapper">
+              <button className="joinBtn2" type="button">Join Raise Studio</button>
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   );
