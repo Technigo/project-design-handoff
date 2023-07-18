4f49fc64681f1abb451d35d2a8bd8449c4a0369f
Emma Engvall <emma.engvall@ica.se>
2023-07-18 20:32:05 +0200

    updated CSS for Quizzes component

--- src/components/Quizzes.js
+++ src/components/Quizzes.js
@@ -3,7 +3,7 @@ import './Quizzes.css';
 
 export const Quizzes = () => {
   return (
-    <div className="quiz-container">
+    <div className="quizWrapper">
       <h5 className="quizheader">Find Your Barre</h5>
       <p>Take Our Quiz to see which Class that Fits You!</p>
       <button className="quizBtn" type="button">Barre Quiz</button>
